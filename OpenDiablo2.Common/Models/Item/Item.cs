a60d141cb97f807052ea591416ba0dcd1bef274f
Diego Mazzocchi <grazz@kuro.localdomain>
2018-11-30 01:32:20 -0300

    Some stuf

--- OpenDiablo2.Common/Models/Item/Item.cs
+++ OpenDiablo2.Common/Models/Item/Item.cs
@@ -7,10 +7,13 @@ using System.Threading.Tasks;
 
 namespace OpenDiablo2.Common.Models
 {
+    /**
+     * Base Item class, contains common attributes for all item types.
+     **/
     public abstract class Item
     {
-        public string Code { get; internal set; }
-        public string Name { get; internal set; }
-        public string InvFile { get; internal set; }
+        public string Code { get; internal set; } // Internal code
+        public string Name { get; internal set; } // Item name
+        public string InvFile { get; internal set; } // Sprite used for the inventory and mouse cursor
     }
 }
