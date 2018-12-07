d89917696354352f17b7db6a047cd92b471cb5c6
Kacper Drobny <erexerox@o2.pl>
2018-12-08 00:45:23 +0100

    refactoring of minipanel (#36)

--- OpenDiablo2.Common/Interfaces/UI/IMiniPanel.cs
+++ OpenDiablo2.Common/Interfaces/UI/IMiniPanel.cs
@@ -4,6 +4,10 @@ namespace OpenDiablo2.Common.Interfaces
 {
     public interface IMiniPanel : IDisposable
     {
+        bool IsLeftPanelVisible { get; }
+        bool IsRightPanelVisible { get; }
+
+        void OnMenuToggle(bool isToggled);
         void Render();
         void Update();
     }
