52ae254b0a0417cd06f613f1543f69ae8953a19b
Tim Sarbin <tim.sarbin@gmail.com>
2018-12-08 16:37:17 -0500

    Added music support

--- OpenDiablo2.SDL2/SDL2MusicPlayer.cs
+++ OpenDiablo2.SDL2/SDL2MusicPlayer.cs
@@ -25,16 +25,17 @@ namespace OpenDiablo2.SDL2_
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private IntPtr music = IntPtr.Zero;
+        private int musicChannel;
 
         public SDL2MusicPlayer()
         {
-            if (SDL_mixer.Mix_OpenAudio(22050, SDL_mixer.MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+            if (SDL_mixer.Mix_OpenAudio(22050, SDL_mixer.MIX_DEFAULT_FORMAT, 2, 1024) < 0)
                 log.Error($"SDL_mixer could not initialize! SDL_mixer Error: {SDL.SDL_GetError()}");
         }
 
         public void PlaySong()
         {
-            SDL_mixer.Mix_PlayChannel(-1, music, 1);
+            musicChannel = SDL_mixer.Mix_PlayChannel(-1, music, 1);
         }
 
         public void LoadSong(Stream data)
@@ -51,6 +52,7 @@ namespace OpenDiablo2.SDL2_
         {
             if (music == IntPtr.Zero)
                 return;
+            SDL_mixer.Mix_HaltChannel(musicChannel);
             SDL_mixer.Mix_FreeChunk(music);
         }
 
@@ -60,21 +62,6 @@ namespace OpenDiablo2.SDL2_
             SDL_mixer.Mix_CloseAudio();
         }
 
-        /*
-
-            musicStream = data;
-            SDL.SDL_AudioSpec want = new SDL.SDL_AudioSpec
-            {
-                freq = 22050,
-                format = SDL.AUDIO_S16LSB,
-                channels = 2,
-                samples = 4096,
-                callback = AudioCallback
-            };
-
-            SDL.SDL_OpenAudio(ref want, out audioSpec);
-            SDL.SDL_PauseAudio(0);
-         */
 
     }
 }
