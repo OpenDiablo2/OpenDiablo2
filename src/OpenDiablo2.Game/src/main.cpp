56b2f146d32f8cc279a3f48104269b87608efa75
Tim Sarbin <tim.sarbin@gmail.com>
2019-02-24 11:15:09 -0500

    More project structure updates

---
+++ src/OpenDiablo2.Game/src/main.cpp
@@ -0,0 +1,47 @@
+#include <memory>
+#include <experimental/filesystem>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <OpenDiablo2.Game/D2Engine.h>
+#include <CLI/CLI.hpp>
+#include <StormLib.h>
+
+int
+main(int    argc,
+	 char** argv)
+{
+	spdlog::set_level(spdlog::level::trace);
+	spdlog::set_pattern("[%^%l%$] %v");
+	spdlog::info("OpenDiablo 2 has started");
+
+	CLI::App app{"OpenDiablo2 - An open source re-implementation of Diablo 2."};
+
+	OpenDiablo2::Game::D2EngineConfig engineConfig;
+	engineConfig.BasePath = std::experimental::filesystem::current_path().string();
+
+	app.add_option("-p,--path", engineConfig.BasePath, "The base path for Diablo 2");
+
+	CLI11_PARSE(app, argc, argv);
+
+	spdlog::info("Base file path is '" + engineConfig.BasePath + "'");
+
+	// Sanity-check that files are where we expect them to be...
+	auto testFilePath = engineConfig.BasePath + "/d2data.mpq";
+
+	HANDLE mpq = nullptr;
+
+	if (!SFileOpenArchive(("flat-file:" + testFilePath).c_str(), 0, STREAM_FLAG_READ_ONLY, &mpq)) {
+		spdlog::error("Diablo 2 content files were not detected. Please make sure the base path is properly set!");
+		exit(0);
+	}
+	SFileCloseFile(mpq);
+	spdlog::info("Content files were located, starting engine.");
+
+	// Start up the engine
+	auto engine = std::make_unique<OpenDiablo2::Game::D2Engine>(engineConfig);
+	engine->Run();
+
+	return 0;
+}
+
+
---
+++ src/OpenDiablo2.SDL2/src/D2Graphics.cpp
@@ -0,0 +1,54 @@
+#include <OpenDiablo2.System/D2Graphics.h>
+#include <spdlog/spdlog.h>
+#include <string>
+#include <SDL2/SDL.h>
+
+
+#include "OpenDiablo2.System/D2Graphics.h"
+
+namespace OpenDiablo2 {
+	namespace System {
+
+		D2Graphics::D2Graphics() {
+			atexit(SDL_Quit);
+
+			if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+				spdlog::error("Could not initialize sdl2: " + std::string(SDL_GetError()));
+				exit(1);
+			}
+		}
+
+		void
+		D2Graphics::Clear() {
+			SDL_RenderClear(renderer.get());
+		}
+
+		void
+		D2Graphics::InitializeWindow() {
+			spdlog::debug("Initializing SDL window");
+			window = std::unique_ptr<SDL_Window, SDLWindowDestroyer>(SDL_CreateWindow("OpenDiablo 2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			  800, 600, SDL_WINDOW_SHOWN));
+			if (window == nullptr) {
+				spdlog::error("Could not create sdl2 window: " + std::string(SDL_GetError()));
+				SDL_Quit();
+				exit(1);
+			}
+
+			spdlog::debug("Destroying SDL renderer");
+			renderer = std::unique_ptr<SDL_Renderer, SDLRendererDestroyer>(SDL_CreateRenderer(window.get(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC));
+			if (renderer == nullptr){
+				spdlog::error("Could not create sdl2 window: " + std::string(SDL_GetError()));
+				SDL_Quit();
+				exit(1);
+			}
+		}
+
+		void
+		D2Graphics::Present() {
+			SDL_RenderPresent(renderer.get());
+		}
+
+
+	}
+}
+
