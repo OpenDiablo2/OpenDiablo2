4ec7dbef43de06eb1c42001ad2e06ce4d8634e9a
Tim Sarbin <tim.sarbin@gmail.com>
2019-02-24 23:36:34 -0500

    Strongly typed palette entries.

--- src/OpenDiablo2.Game/include/OpenDiablo2.Game/Common/Palette.h
+++ src/OpenDiablo2.Game/include/OpenDiablo2.Game/Common/Palette.h
@@ -4,27 +4,27 @@
 #include <string>
 
 namespace OpenDiablo2::Game::Common {
-
 class Palette {
 public:
-	const std::string Act1 = "ACT1";
-	const std::string Act2 = "ACT2";
-	const std::string Act3 = "ACT3";
-	const std::string Act4 = "ACT4";
-	const std::string Act5 = "ACT5";
-	const std::string EndGame = "EndGame";
-	const std::string EndGame2 = "EndGame2";
-	const std::string Fechar = "fechar";
-	const std::string Loading = "loading";
-	const std::string Menu0 = "Menu0";
-	const std::string Menu1 = "menu1";
-	const std::string Menu2 = "menu2";
-	const std::string Menu3 = "menu3";
-	const std::string Menu4 = "menu4";
-	const std::string Sky = "Sky";
-	const std::string Static = "STATIC";
-	const std::string Trademark = "Trademark";
-	const std::string Units = "Units";
+	typedef std::string Entry;
+	const Entry Act1 = "ACT1";
+	const Entry Act2 = "ACT2";
+	const Entry Act3 = "ACT3";
+	const Entry Act4 = "ACT4";
+	const Entry Act5 = "ACT5";
+	const Entry EndGame = "EndGame";
+	const Entry EndGame2 = "EndGame2";
+	const Entry Fechar = "fechar";
+	const Entry Loading = "loading";
+	const Entry Menu0 = "Menu0";
+	const Entry Menu1 = "menu1";
+	const Entry Menu2 = "menu2";
+	const Entry Menu3 = "menu3";
+	const Entry Menu4 = "menu4";
+	const Entry Sky = "Sky";
+	const Entry Static = "STATIC";
+	const Entry Trademark = "Trademark";
+	const Entry Units = "Units";
 private:
 	Palette() {}
 };
