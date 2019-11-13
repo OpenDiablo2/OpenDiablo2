f838846c1986e28950176f000b0316b02b34079f
liberodark <liberodark@gmail.com>
2019-11-13 01:06:49 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,8 @@
 # About: Build OpenDiablo 2 automatically
 # Author: liberodark
 # License: GNU GPLv3
-set -eu
 
-version="0.0.2"
+version="0.0.4"
 
 echo "OpenDiablo 2 Build Script $version"
 
@@ -13,41 +12,49 @@ echo "OpenDiablo 2 Build Script $version"
 # RETRIEVE ARGUMENTS FROM THE MANIFEST AND VAR
 #=================================================
 
-distro=$(sed -n 's#PRETTY_NAME="\([^"]*\)"#\1#p' /etc/os-release)
+distribution=$(cat /etc/*release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 's/["]//g' | awk '{print $1}')
 
-# Check OS & go
 
-if ! command -v go >/dev/null
-then
-	echo "Install Go for OpenDiablo 2 ($distro)? y/n"
+go_install(){
+  # Check OS & go
+
+  if ! command -v go; then
+
+  	echo "Install Go for OpenDiablo 2 ($distribution)? y/n"
 	read -r choice
 	[ "$choice" != y ] && [ "$choice" != Y ] && exit
 
-	case "$(echo "$distro" | tr '[:upper:]' '[:lower:]')" in
-		centos|red\ hat|suse|oracle)
-			sudo yum install -y go >/dev/null 2>&1
-			;;
-		fedora)
-			sudo dnf install -y go >/dev/null 2>&1
-			;;
-		debian|ubuntu|deepin)
-			sudo apt-get update >/dev/null 2>&1
-			sudo apt-get install -y go --force-yes >/dev/null 2>&1
-			;;
-		manjaro|arch\ linux)
-			sudo pacman -S go --noconfirm >/dev/null 2>&1
-			;;
-		gentoo/linux)
-			sudo emerge --ask n go /dev/null 2>&1
-			;;
-		*)
-			echo "$distro: unsupported distribution, please install Go by yourself"
-			exit 1
-			;;
-	esac
+    if [[ "$distribution" = CentOS || "$distribution" = CentOS || "$distribution" = Red\ Hat || "$distribution" = Suse || "$distribution" = Oracle ]]; then
+      wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
+	  sudo tar -C /usr/local -xzf go*.linux-amd64.tar.gz
+	  rm go*.linux-amd64.tar.gz
+	  export PATH=$PATH:/usr/local/go/bin
+	  sudo dnf install -y libX11-devel libXcursor-devel libXrandr-devel libXinerama-devel mesa-libGL-devel
+      
+    elif [[ "$distribution" = Fedora ]]; then
+      wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
+	  sudo tar -C /usr/local -xzf go*.linux-amd64.tar.gz
+	  rm go*.linux-amd64.tar.gz
+	  export PATH=$PATH:/usr/local/go/bin
+	  sudo dnf install -y libX11-devel libXcursor-devel libXrandr-devel libXinerama-devel mesa-libGL-devel
+    
+    elif [[ "$distribution" = Debian || "$distribution" = Ubuntu || "$distribution" = Deepin ]]; then
+      apt-get update
+      apt-get install -y make autoconf automake gcc libc6 libmcrypt-dev libssl-dev openssl packagekit --force-yes
+      
+    elif [[ "$distribution" = Gentoo ]]; then
+      sudo emerge --ask n go
+      
+    elif [[ "$distribution" = Manjaro || "$distribution" = Arch\ Linux ]]; then
+      sudo pacman -S go --noconfirm
+
+    fi
 fi
+}
 
 # Build
+echo "Check Go"
+go_install
 echo "Build OpenDiablo 2"
 go get
 go build
