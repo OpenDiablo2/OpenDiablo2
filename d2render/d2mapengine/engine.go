a56bd24b80dbaa4a6b59c0a685370c5d2d2e648a
Ziemas <ziemas@ziemas.se>
2019-11-14 06:46:38 +0100

    Render in two passes
    To avoid drawing over objects.

--- d2render/d2mapengine/engine.go
+++ d2render/d2mapengine/engine.go
@@ -21,16 +21,22 @@ import (
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
+type RegionTile struct {
+	tileX, tileY int // tile coordinates
+	offX, offY   int // world space coordinates of tile origin
+}
+
 type EngineRegion struct {
 	Rect   d2common.Rectangle
 	Region *Region
+	Tiles  []*RegionTile
 }
 
 type Engine struct {
 	soundManager *d2audio.Manager
 	gameState    *d2core.GameState
 	fileProvider d2interface.FileProvider
-	regions      []EngineRegion
+	regions      []*EngineRegion
 	OffsetX      float64
 	OffsetY      float64
 	ShowTiles    int
@@ -41,7 +47,7 @@ func CreateMapEngine(gameState *d2core.GameState, soundManager *d2audio.Manager,
 		gameState:    gameState,
 		soundManager: soundManager,
 		fileProvider: fileProvider,
-		regions:      make([]EngineRegion, 0),
+		regions:      make([]*EngineRegion, 0),
 	}
 	return result
 }
@@ -49,7 +55,7 @@ func CreateMapEngine(gameState *d2core.GameState, soundManager *d2audio.Manager,
 func (v *Engine) GenerateMap(regionType d2enum.RegionIdType, levelPreset int) {
 	randomSource := rand.NewSource(v.gameState.Seed)
 	region := LoadRegion(randomSource, regionType, levelPreset, v.fileProvider)
-	v.regions = append(v.regions, EngineRegion{
+	v.regions = append(v.regions, &EngineRegion{
 		Rect:   d2common.Rectangle{0, 0, int(region.TileWidth), int(region.TileHeight)},
 		Region: region,
 	})
@@ -59,22 +65,26 @@ func (v *Engine) GenerateAct1Overworld() {
 	v.soundManager.PlayBGM("/data/global/music/Act1/town1.wav") // TODO: Temp stuff here
 	randomSource := rand.NewSource(v.gameState.Seed)
 	region := LoadRegion(randomSource, d2enum.RegionAct1Town, 1, v.fileProvider)
-	v.regions = append(v.regions, EngineRegion{
+	v.regions = append(v.regions, &EngineRegion{
 		Rect:   d2common.Rectangle{0, 0, int(region.TileWidth), int(region.TileHeight)},
 		Region: region,
 	})
 	if strings.Contains(region.RegionPath, "E1") {
 		region2 := LoadRegion(randomSource, d2enum.RegionAct1Town, 2, v.fileProvider)
-		v.regions = append(v.regions, EngineRegion{
+		v.regions = append(v.regions, &EngineRegion{
 			Rect:   d2common.Rectangle{int(region.TileWidth - 1), 0, int(region2.TileWidth), int(region2.TileHeight)},
 			Region: region2,
 		})
 	} else if strings.Contains(region.RegionPath, "S1") {
 		region2 := LoadRegion(randomSource, d2enum.RegionAct1Town, 3, v.fileProvider)
-		v.regions = append(v.regions, EngineRegion{
+		v.regions = append(v.regions, &EngineRegion{
 			Rect:   d2common.Rectangle{0, int(region.TileHeight - 1), int(region2.TileWidth), int(region2.TileHeight)},
 			Region: region2,
 		})
+	}
+
+	for _, region := range v.regions {
+		v.GenTiles(region)
 	}
 
 	sx, sy := d2helper.IsoToScreen(int(region.StartX), int(region.StartY), 0, 0)
@@ -90,52 +100,56 @@ func (v *Engine) GetRegionAt(x, y int) *EngineRegion {
 		if !region.Rect.IsInRect(x, y) {
 			continue
 		}
-		return &region
+		return region
 	}
 	return nil
 }
 
 func (v *Engine) Render(target *ebiten.Image) {
 	for _, region := range v.regions {
-		v.RenderRegion(region, target)
+		v.RenderRegion(*region, target)
 	}
 }
 
-func (v *Engine) RenderRegion(region EngineRegion, target *ebiten.Image) {
+func (v *Engine) GenTiles(region *EngineRegion) {
 	for y := 0; y < int(region.Region.TileHeight); y++ {
 		offX := -((y + region.Rect.Top) * 80) + (region.Rect.Left * 80)
 		offY := ((y + region.Rect.Top) * 40) + (region.Rect.Left * 40)
 		for x := 0; x < int(region.Region.TileWidth); x++ {
-			sx, sy := d2helper.IsoToScreen(x+region.Rect.Left, y+region.Rect.Top, int(v.OffsetX), int(v.OffsetY))
-			if sx > -160 && sy > -160 && sx <= 880 && sy <= 1000 {
-				v.RenderTile(region.Region, offX, offY, x, y, target)
-			}
+			region.Tiles = append(region.Tiles, &RegionTile{
+				tileX: x,
+				tileY: y,
+				offX:  offX,
+				offY:  offY,
+			})
+
 			offX += 80
 			offY += 40
 		}
 	}
 }
 
-func (v *Engine) RenderTile(region *Region, offX, offY, x, y int, target *ebiten.Image) {
+func (v *Engine) RenderRegion(region EngineRegion, target *ebiten.Image) {
+	var tilesToRender []RegionTile
+
+	for _, tile := range region.Tiles {
+		sx, sy := d2helper.IsoToScreen(tile.tileX+region.Rect.Left, tile.tileY+region.Rect.Top, int(v.OffsetX), int(v.OffsetY))
+		if sx > -160 && sy > -160 && sx <= 880 && sy <= 1000 {
+			tilesToRender = append(tilesToRender, *tile)
+		}
+	}
+
+	for _, tile := range tilesToRender {
+		v.RenderTile(region.Region, tile.offX, tile.offY, tile.tileX, tile.tileY, target)
+	}
+
+	for _, tile := range tilesToRender {
+		v.RenderTileObjects(region.Region, tile.offX, tile.offY, tile.tileX, tile.tileY, target)
+	}
+}
+
+func (v *Engine) RenderTileObjects(region *Region, offX, offY, x, y int, target *ebiten.Image) {
 	tile := region.DS1.Tiles[y][x]
-	for i := range tile.Floors {
-		if tile.Floors[i].Hidden || tile.Floors[i].Prop1 == 0 {
-			continue
-		}
-		region.RenderTile(offX+int(v.OffsetX), offY+int(v.OffsetY), x, y, d2enum.RegionLayerTypeFloors, i, target)
-	}
-	for i := range tile.Shadows {
-		if tile.Shadows[i].Hidden || tile.Shadows[i].Prop1 == 0 {
-			continue
-		}
-		region.RenderTile(offX+int(v.OffsetX), offY+int(v.OffsetY), x, y, d2enum.RegionLayerTypeShadows, i, target)
-	}
-	for i := range tile.Walls {
-		if tile.Walls[i].Hidden || tile.Walls[i].Orientation == 15 || tile.Walls[i].Orientation == 10 || tile.Walls[i].Orientation == 11 || tile.Walls[i].Orientation == 0 {
-			continue
-		}
-		region.RenderTile(offX+int(v.OffsetX), offY+int(v.OffsetY), x, y, d2enum.RegionLayerTypeWalls, i, target)
-	}
 	for _, obj := range region.AnimationEntities {
 		if int(math.Floor(obj.LocationX)) == x && int(math.Floor(obj.LocationY)) == y {
 			obj.Render(target, offX+int(v.OffsetX), offY+int(v.OffsetY))
@@ -174,4 +188,27 @@ func (v *Engine) RenderTile(region *Region, offX, offY, x, y int, target *ebiten
 			}
 		}
 	}
+}
+
+func (v *Engine) RenderTile(region *Region, offX, offY, x, y int, target *ebiten.Image) {
+	tile := region.DS1.Tiles[y][x]
+	for i := range tile.Floors {
+		if tile.Floors[i].Hidden || tile.Floors[i].Prop1 == 0 {
+			continue
+		}
+		region.RenderTile(offX+int(v.OffsetX), offY+int(v.OffsetY), x, y, d2enum.RegionLayerTypeFloors, i, target)
+	}
+	for i := range tile.Shadows {
+		if tile.Shadows[i].Hidden || tile.Shadows[i].Prop1 == 0 {
+			continue
+		}
+		region.RenderTile(offX+int(v.OffsetX), offY+int(v.OffsetY), x, y, d2enum.RegionLayerTypeShadows, i, target)
+	}
+	for i := range tile.Walls {
+		if tile.Walls[i].Hidden || tile.Walls[i].Orientation == 15 || tile.Walls[i].Orientation == 10 || tile.Walls[i].Orientation == 11 || tile.Walls[i].Orientation == 0 {
+			continue
+		}
+		region.RenderTile(offX+int(v.OffsetX), offY+int(v.OffsetY), x, y, d2enum.RegionLayerTypeWalls, i, target)
+	}
+
 }
