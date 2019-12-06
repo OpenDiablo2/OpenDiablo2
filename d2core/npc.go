ca220256658521d7beeeba3b72be9444e33dcedd
Nicholas Eden <nicholas.eden@mheducation.com>
2019-12-05 23:05:11 -0800

    Setup NPCs to follow paths
    Change location to contain canonical location, add field to get rounded location for tile rendering.
    If NPC has path, loop through path.

--- d2core/npc.go
+++ d2core/npc.go
@@ -11,19 +11,36 @@ import (
 
 type NPC struct {
 	AnimatedEntity d2render.AnimatedEntity
+	HasPaths       bool
 	Paths          []d2common.Path
+	path           int
 }
 
 func CreateNPC(x, y int32, object *d2datadict.ObjectLookupRecord, fileProvider d2interface.FileProvider, direction int) *NPC {
 	result := &NPC{
 		AnimatedEntity: d2render.CreateAnimatedEntity(x, y, object, fileProvider, d2enum.Units),
+		HasPaths:       false,
 	}
 	result.AnimatedEntity.SetMode(object.Mode, object.Class, direction)
 	return result
 }
 
+func (v *NPC) Path() d2common.Path {
+	return v.Paths[v.path]
+}
+
+func (v *NPC) NextPath() d2common.Path {
+	v.path++
+	if v.path == len(v.Paths) {
+		v.path = 0
+	}
+
+	return v.Paths[v.path]
+}
+
 func (v *NPC) SetPaths(paths []d2common.Path) {
 	v.Paths = paths
+	v.HasPaths = len(paths) > 0
 }
 
 func (v *NPC) Render(target *ebiten.Image, offsetX, offsetY int) {
