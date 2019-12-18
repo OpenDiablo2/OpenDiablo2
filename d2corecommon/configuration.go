778dba2a222cf3deb21d0a49ccf2b54ae18f8a6a
Alex Yatskov <alex@foosoft.net>
2019-12-17 17:49:55 -0800

    Cleanup config file loading, move defaults to code

--- d2corecommon/configuration.go
+++ d2corecommon/configuration.go
@@ -2,14 +2,10 @@ package d2corecommon
 
 import (
 	"encoding/json"
-	"io/ioutil"
-	"log"
 	"os"
+	"os/user"
 	"path"
 	"runtime"
-	"strings"
-
-	"github.com/mitchellh/go-homedir"
 )
 
 // Configuration defines the configuration for the engine, loaded from config.json
@@ -23,44 +19,88 @@ type Configuration struct {
 	VsyncEnabled    bool
 	MpqPath         string
 	MpqLoadOrder    []string
-	MpqMacLoadOrder []string
 	SfxVolume       float64
 	BgmVolume       float64
 }
 
-// ConfigBasePath is used for tests to find the base config json file
-var ConfigBasePath = "./"
-
 func LoadConfiguration() *Configuration {
-	configJSON, err := ioutil.ReadFile(path.Join(ConfigBasePath, "config.json"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	var config Configuration
-	err = json.Unmarshal(configJSON, &config)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if runtime.GOOS == "darwin" {
-		if config.MpqPath[0] != '/' {
-			config.MpqPath = "/Applications/Diablo II/"
-		}
-		config.MpqLoadOrder = config.MpqMacLoadOrder
-	} else {
-		// Path fixup for wine-installed diablo 2 in linux
-		if config.MpqPath[0] != '/' {
-			if _, err := os.Stat(config.MpqPath); os.IsNotExist(err) {
-				homeDir, _ := homedir.Dir()
-				newPath := strings.ReplaceAll(config.MpqPath, `C:\`, homeDir+"/.wine/drive_c/")
-				newPath = strings.ReplaceAll(newPath, "C:/", homeDir+"/.wine/drive_c/")
-				newPath = strings.ReplaceAll(newPath, `\`, "/")
-				if _, err := os.Stat(newPath); !os.IsNotExist(err) {
-					config.MpqPath = newPath
-				}
-			}
+	configFile, err := os.Open("config.json")
+	defer configFile.Close()
+
+	if err == nil {
+		var config Configuration
+		decoder := json.NewDecoder(configFile)
+		if err := decoder.Decode(&config); err == nil {
+			return &config
 		}
 	}
-	return &config
+
+	return getDefaultConfiguration()
 }
 
+func (c *Configuration) Save() error {
+	configFile, err := os.Create("config.json")
+	if err != nil {
+		return err
+	}
 
+	encoder := json.NewEncoder(configFile)
+	encoder.SetIndent("", "    ")
+	if err := encoder.Encode(c); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getDefaultConfiguration() *Configuration {
+	config := &Configuration{
+		Language:        "ENG",
+		FullScreen:      false,
+		Scale:           1,
+		TicksPerSecond:  -1,
+		RunInBackground: true,
+		VsyncEnabled:    true,
+		SfxVolume:       1.0,
+		BgmVolume:       0.3,
+		MpqPath:         "C:/Program Files (x86)/Diablo II",
+		MpqLoadOrder: []string{
+			"Patch_D2.mpq",
+			"d2exp.mpq",
+			"d2xmusic.mpq",
+			"d2xtalk.mpq",
+			"d2xvideo.mpq",
+			"d2data.mpq",
+			"d2char.mpq",
+			"d2music.mpq",
+			"d2sfx.mpq",
+			"d2video.mpq",
+			"d2speech.mpq",
+		},
+	}
+
+	switch runtime.GOOS {
+	case "darwin":
+		config.MpqPath = "/Applications/Diablo II/"
+		config.MpqLoadOrder = []string{
+			"Diablo II Patch",
+			"Diablo II Expansion Data",
+			"Diablo II Expansion Movies",
+			"Diablo II Expansion Music",
+			"Diablo II Expansion Speech",
+			"Diablo II Game Data",
+			"Diablo II Graphics",
+			"Diablo II Movies",
+			"Diablo II Music",
+			"Diablo II Sounds",
+			"Diablo II Speech",
+		}
+	case "linux":
+		usr, err := user.Current()
+		if err == nil {
+			config.MpqPath = path.Join(usr.HomeDir, ".wine/drive_c/Program Files (x86)/Diablo II")
+		}
+	}
+
+	return config
+}
