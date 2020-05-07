898fa8b4a301e12e4ea2aa908faeb05a842bc567
dk <dknuth0101@gmail.com>
2020-05-07 10:44:00 -0700

    d2common: item affix datadict and records
    Item Affixes are defined in `/data/global/excel/Magic{Prefix,Suffix}.txt`
    Rare and Unique Pre/Suffixes seem to be for monsters, not items.
    
    d2common: item affixes only
    
    removed Rare/Unique Prefix/Suffix as those are related to monsters, not items.
    
    removed debug print from item_affix.go
    
    changed item affix type names for clarity, removed debug print from data_dictionary

---
+++ d2common/d2enum/item_affix_type.go
@@ -0,0 +1,16 @@
+package d2enum
+
+type ItemAffixSuperType int
+type ItemAffixSubType int
+
+const (
+	ItemAffixPrefix ItemAffixSuperType = iota
+	ItemAffixSuffix
+)
+
+const (
+	ItemAffixCommon ItemAffixSubType = iota
+	ItemAffixMagic
+	ItemAffixRare
+	ItemAffixUnique
+)
