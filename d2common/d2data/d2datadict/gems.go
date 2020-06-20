b50407eb72fda53d4639ce7bf647120dbc95300d
Jonathan Rosenboim <masterfqf@gmail.com>
2020-06-20 11:34:35 +0300

    Add GemsRecord struct

---
+++ d2common/d2data/d2datadict/gems.go
@@ -0,0 +1,44 @@
+package d2datadict
+
+type GemsRecord struct {
+	Name             string
+	Letter           string
+	Transform        string
+	Code             string
+	Nummods          int
+	WeaponMod1Code   string
+	WeaponMod1Param  int
+	WeaponMod1Min    int
+	WeaponMod1Max    int
+	WeaponMod2Code   string
+	WeaponMode2Param int
+	WeaponMod2Min    int
+	WeaponMod2Max    int
+	WeaponMod3Code   string
+	WeaponMod3Min    int
+	WeaponMod3Max    int
+	HelmMod1Code     string
+	HelmMod1Param    int
+	HelmMod1Min      int
+	HelmMod1Max      int
+	HelmMod2Code     string
+	HelmMod2Param    int
+	HelmMod2Min      int
+	HelmMod2Max      int
+	HelmMod3Code     string
+	HelmMod3Param    int
+	HelmMod3Min      int
+	HelmMod3Max      int
+	ShieldMod1Code   string
+	ShieldMod1Param  int
+	ShieldMod1Min    int
+	ShieldMod1Max    int
+	ShieldMod2Code   string
+	ShieldMod2Param  int
+	ShieldMod2Min    int
+	ShieldMod2Max    int
+	ShieldMod3Code   string
+	ShieldMod3Param  int
+	ShieldMod3Min    int
+	ShieldMod3Max    int
+}
