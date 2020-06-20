8ee81a1f4dc0c6fe6abc3f339e6e507eb7c17bd1
Jonathan Rosenboim <masterfqf@gmail.com>
2020-06-20 14:58:27 +0300

    Add LoadHireling func implementation

--- d2common/d2data/d2datadict/hireling.go
+++ d2common/d2data/d2datadict/hireling.go
@@ -1,5 +1,7 @@
 package d2datadict
 
+import "github.com/OpenDiablo2/OpenDiablo2/d2common"
+
 type HirelingRecord struct {
 	Hireling        string
 	SubType         string
@@ -73,4 +75,86 @@ type HirelingRecord struct {
 	Torso           int
 	Weapon          int
 	Shield          int
+}
+
+func LoadHireling(file []byte) {
+	d := d2common.LoadDataDictionary(string(file))
+	var hirelings []*HirelingRecord
+	for idx, _ := range d.Data {
+		hireling := &HirelingRecord{
+			Hireling:        d.GetString("Hireling", idx),
+			SubType:         d.GetString("SubType", idx),
+			Id:              d.GetNumber("Id", idx),
+			Class:           d.GetNumber("Class", idx),
+			Act:             d.GetNumber("Act", idx),
+			Difficulty:      d.GetNumber("Difficulty", idx),
+			Level:           d.GetNumber("Level", idx),
+			Seller:          d.GetNumber("Seller", idx),
+			NameFirst:       d.GetString("NameFirst", idx),
+			NameLast:        d.GetString("NameLast", idx),
+			Gold:            d.GetNumber("Gold", idx),
+			ExpPerLvl:       d.GetNumber("Exp/Lvl", idx),
+			HP:              d.GetNumber("HP", idx),
+			HPPerLvl:        d.GetNumber("HP/Lvl", idx),
+			Defense:         d.GetNumber("Defense", idx),
+			DefPerLvl:       d.GetNumber("Id", idx),
+			Str:             d.GetNumber("Str", idx),
+			StrPerLvl:       d.GetNumber("Str/Lvl", idx),
+			Dex:             d.GetNumber("Dex", idx),
+			DexPerLvl:       d.GetNumber("Dex/Lvl", idx),
+			AR:              d.GetNumber("AR", idx),
+			ARPerLvl:        d.GetNumber("AR/Lvl", idx),
+			Share:           d.GetNumber("Share", idx),
+			DmgMin:          d.GetNumber("DmgMin", idx),
+			DmgMax:          d.GetNumber("DmgMax", idx),
+			DmgPerLvl:       d.GetNumber("DmgPerLvl", idx),
+			Resist:          d.GetNumber("Resist", idx),
+			ResistPerLvl:    d.GetNumber("Resist/Lvl", idx),
+			WType1:          d.GetString("WType1", idx),
+			WType2:          d.GetString("WType2", idx),
+			HireDesc:        d.GetString("HireDesc", idx),
+			DefaultChance:   d.GetNumber("DefaultChance", idx),
+			Skill1:          d.GetString("Skill1", idx),
+			Mode1:           d.GetNumber("Mode1", idx),
+			Chance1:         d.GetNumber("Chance1", idx),
+			ChancePerLevel1: d.GetNumber("ChancePerLevel1", idx),
+			Level1:          d.GetNumber("Level1", idx),
+			LvlPerLvl1:      d.GetNumber("LvlPerLvl1", idx),
+			Skill2:          d.GetString("Skill2", idx),
+			Mode2:           d.GetNumber("Mode2", idx),
+			Chance2:         d.GetNumber("Chance2", idx),
+			ChancePerLevel2: d.GetNumber("ChancePerLevel2", idx),
+			Level2:          d.GetNumber("Level2", idx),
+			LvlPerLvl2:      d.GetNumber("LvlPerLvl2", idx),
+			Skill3:          d.GetString("Skill3", idx),
+			Mode3:           d.GetNumber("Mode3", idx),
+			Chance3:         d.GetNumber("Chance3", idx),
+			ChancePerLevel3: d.GetNumber("ChancePerLevel3", idx),
+			Level3:          d.GetNumber("Level3", idx),
+			LvlPerLvl3:      d.GetNumber("LvlPerLvl3", idx),
+			Skill4:          d.GetString("Skill4", idx),
+			Mode4:           d.GetNumber("Mode4", idx),
+			Chance4:         d.GetNumber("Chance4", idx),
+			ChancePerLevel4: d.GetNumber("ChancePerLevel4", idx),
+			Level4:          d.GetNumber("Level4", idx),
+			LvlPerLvl4:      d.GetNumber("LvlPerLvl4", idx),
+			Skill5:          d.GetString("Skill5", idx),
+			Mode5:           d.GetNumber("Mode5", idx),
+			Chance5:         d.GetNumber("Chance5", idx),
+			ChancePerLevel5: d.GetNumber("ChancePerLevel5", idx),
+			Level5:          d.GetNumber("Level5", idx),
+			LvlPerLvl5:      d.GetNumber("LvlPerLvl5", idx),
+			Skill6:          d.GetString("Skill6", idx),
+			Mode6:           d.GetNumber("Mode6", idx),
+			Chance6:         d.GetNumber("Chance6", idx),
+			ChancePerLevel6: d.GetNumber("ChancePerLevel6", idx),
+			Level6:          d.GetNumber("Level6", idx),
+			LvlPerLvl6:      d.GetNumber("LvlPerLvl6", idx),
+			Head:            d.GetNumber("Head", idx),
+			Torso:           d.GetNumber("Torso", idx),
+			Weapon:          d.GetNumber("Weapon", idx),
+			Shield:          d.GetNumber("Shield", idx),
+		}
+		hirelings = append(hirelings, hireling)
+	}
 }
