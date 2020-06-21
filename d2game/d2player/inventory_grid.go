cdd47f50b6609c09f9ad44975bde9b6a001ca799
Jonathan Rosenboim <masterfqf@gmail.com>
2020-06-21 21:39:57 +0300

    Add initialization to all equipped slots types.

--- d2game/d2player/inventory_grid.go
+++ d2game/d2player/inventory_grid.go
@@ -51,8 +51,53 @@ func NewItemGrid(width int, height int, originX int, originY int) *ItemGrid {
 		equipmentSlots: map[string]EquipmentSlot{
 			"LeftHand": {
 				Item: nil,
-				x:    400,
+				x:    438,
 				y:    200,
+			},
+			"RightHand": {
+				Item: nil,
+				x:    665,
+				y:    200,
+			},
+			"Helmet": {
+				Item: nil,
+				x:    549,
+				y:    83,
+			},
+			"Amulet": {
+				Item: nil,
+				x:    613,
+				y:    103,
+			},
+			"Chest": {
+				Item: nil,
+				x:    546,
+				y:    155,
+			},
+			"Belt": {
+				Item: nil,
+				x:    548,
+				y:    250,
+			},
+			"LeftRing": {
+				Item: nil,
+				x:    501,
+				y:    250,
+			},
+			"RightRing": {
+				Item: nil,
+				x:    613,
+				y:    250,
+			},
+			"Gloves": {
+				Item: nil,
+				x:    429,
+				y:    252,
+			},
+			"Shoes": {
+				Item: nil,
+				x:    661,
+				y:    252,
 			},
 		},
 	}
