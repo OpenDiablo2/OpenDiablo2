91f44768f7f6f85d48c7456c1f346c6d28c7d894
dknuth <dknuth0101@gmail.com>
2020-06-21 13:38:34 -0700

    adding comments from PK forums in the added d2enum files

--- d2common/d2enum/level_generation_types.go
+++ d2common/d2enum/level_generation_types.go
@@ -1,6 +1,12 @@
 package d2enum
 
-// from levels.txt
+// from levels.txt, field `DrlgType`
+// https://d2mods.info/forum/kb/viewarticle?a=301
+
+// Setting for Level Generation: You have 3 possibilities here:
+// 1 Random Maze
+// 2 Preset Area
+// 3 Wilderness level
 
 type LevelGenerationType int
 
