01f5a7ff8fe0538d7f2ba6b94fe38151f68f8569
unknown <danhale87@gmail.com>
2020-06-22 20:42:12 +0100

    Removed CubeRecipeRequirement added CubeRecipeResult

--- d2common/d2data/d2datadict/cubemain.go
+++ d2common/d2data/d2datadict/cubemain.go
@@ -26,8 +26,14 @@ type CubeRecipeRecord struct {
 	// Version specifies whether the recipe is old classic, new classic or expansion.
 	Version int // 0, 1, 100 = old cl, new cl, expansion
 
-	// Requirement holds the parameters for a comparison which can cause the recipe to be skipped or fail.
-	Requirement CubeRecipeRequirement
+	// The following three values form a comparison: if <StatID> <Operation> <Value> then recipe is allowed.
+	//
+	// Parameter is an ID value from the ItemStatsCost data set specifying the stat to compare. Whether this references a player or item stat depends on the Operator.
+	StatID int
+	// Operation is a number describing the comparison operator and the action to take if it evaluates to true. See Appendix A in the linked article and note that 1, 2, 27 and 28 are unconventional.
+	Operation int // 1 - 28
+	// Value is the number the stat is compared against.
+	Value int
 
 	// Class Can be used to make recipes class specific. Example class codes given are:
 	// ama bar pal nec sor dru ass
@@ -39,127 +45,73 @@ type CubeRecipeRecord struct {
 	// NumInputs is the total count of input items required, including counts in item stacks.
 	NumInputs int
 
-	// Inputs is a list of CubeRecipeInput structs representing the data from the input 1-7 field
-	Inputs []CubeRecipeInput
+	// Inputs is the actual recipe, a collection of items/stacks with parameters required to obtain the items defined in Outputs.
+	Inputs []CubeRecipeItem
 
-	//output
-	//lvl
-	//plvl
-	//ilvl
-	//mod 1
-	//mod 1 chance
-	//mod 1 param
-	//mod 1 min
-	//mod 1 max
-	//mod 2
-	//mod 2 chance
-	//mod 2 param
-	//mod 2 min
-	//mod 2 max
-	//mod 3
-	//mod 3 chance
-	//mod 3 param
-	//mod 3 min
-	//mod 3 max
-	//mod 4
-	//mod 4 chance
-	//mod 4 param
-	//mod 4 min
-	//mod 4 max
-	//mod 5
-	//mod 5 chance
-	//mod 5 param
-	//mod 5 min
-	//mod 5 max
-	//output b
-	//b lvl
-	//b plvl
-	//b ilvl
-	//b mod 1
-	//b mod 1 chance
-	//b mod 1 param
-	//b mod 1 min
-	//b mod 1 max
-	//b mod 2
-	//b mod 2 chance
-	//b mod 2 param
-	//b mod 2 min
-	//b mod 2 max
-	//b mod 3
-	//b mod 3 chance
-	//b mod 3 param
-	//b mod 3 min
-	//b mod 3 max
-	//b mod 4
-	//b mod 4 chance
-	//b mod 4 param
-	//b mod 4 min
-	//b mod 4 max
-	//b mod 5
-	//b mod 5 chance
-	//b mod 5 param
-	//b mod 5 min
-	//b mod 5 max
-	//output c
-	//c lvl
-	//c plvl
-	//c ilvl
-	//c mod 1
-	//c mod 1 chance
-	//c mod 1 param
-	//c mod 1 min
-	//c mod 1 max
-	//c mod 2
-	//c mod 2 chance
-	//c mod 2 param
-	//c mod 2 min
-	//c mod 2 max
-	//c mod 3
-	//c mod 3 chance
-	//c mod 3 param
-	//c mod 3 min
-	//c mod 3 max
-	//c mod 4
-	//c mod 4 chance
-	//c mod 4 param
-	//c mod 4 min
-	//c mod 4 max
-	//c mod 5
-	//c mod 5 chance
-	//c mod 5 param
-	//c mod 5 min
-	//c mod 5 max
-	//*eol
+	// Outputs are the items created when the recipe is used.
+	Outputs []CubeRecipeResult
 }
 
-// CubeRecipeRequirement represents the op, param and value fields in cubemain.go. These form a comparison: if <Paremeter> <Operator> <Value> then recipe is allowed.
-// See: https://d2mods.info/forum/kb/viewarticle?a=284
-type CubeRecipeRequirement struct {
-	// Parameter is an ID value from the ItemStatsCost data set specifying the stat to compare. Whether this references a player or item stat depends on the Operator.
-	StatID int
-	// Operation is a number describing the comparison operator and the action to take if it evaluates to true. See Appendix A in the linked article and note that 1, 2, 27 and 28 are unconventional.
-	Operation int // 1 - 28
-	// Value is the number the stat is compared against.
-	Value int
+// CubeRecipeResult represents the output, lvl, plvl, ilvl, mod #, mod # chance, mod # param, mod # min, mod # max fields in cubemain.txt. It is an item generated on use of a cube recipe.
+type CubeRecipeResult struct {
+	// Note: The original data allows for only three output items/stacks.
+	// Item the item, with a count and parameters.
+	Item CubeRecipeItem
+
+	// Note that this value force spawns the item at this specific level. Its also used in the formula for the next two fields.
+	Level int // the item level of Item
+
+	// PLevel uses a portion of the players level for the output level.
+	PLevel int
+
+	// ILevel uses a portion of the first input's level for the output level.
+	ILevel int
+
+	// Note: The original data allows for only five properties.
+	// Properties is a list of properties which may be attached to Item.
+	Properties []CubeRecipeItemProperty
 }
 
-// CubeRecipeInput represents the data from the input 1-7 fields in cubemain.txt. Each cell represents a different item to be placed in the cube, stackable items can also have a required count. Originally a comma-delimited string of parameters.
+// CubeRecipeItem represents the data from the input 1-7 fields in cubemain.txt. Each cell represents a different item to be placed in the cube, stackable items can also have a required count. Originally a comma-delimited string of parameters.
 // See: https://d2mods.info/forum/kb/viewarticle?a=284
-type CubeRecipeInput struct {
+type CubeRecipeItem struct {
 	Code   string   // item code e.g. 'weap'
 	Params []string // list of argument parameters e.g. 'sock'
 	Count  int      // required stack count
 }
 
+// CubeRecipeItemProperty represents a property attached to an output item. Properties are defined in properties.txt. See:
+// https://d2mods.info/forum/kb/viewarticle?a=345
+type CubeRecipeItemProperty struct {
+	Code string // the code field from properties.txt
+
+	// Note: I can't find any example value for this so I've made it an int for now
+	Chance int // the chance to apply the property
+
+	// Note: The few examples in cubemain.go are integers, however d2datadict.UniqueItemProperty is a similar struct which handles a similar field that may be a string or an integer.
+	//"the parameter passed on to the associated property, this is used to pass skill IDs, state IDs, monster IDs, montype IDs and the like on to the properties that require them, these fields support calculations."
+	Param int // for properties that use parameters
+
+	Min int // the minimum value of the property stat
+	Max int // the maximum value of the property stat
+}
+
 // CubeRecipes contains all rows CubeMain.txt.
 var CubeRecipes []*CubeRecipeRecord
 
+// The fields and sections in this file have inconsistent naming conventions. These arrays are a simple way to handle them.
+var outputFields = []string{"output", "output b", "output c"}
+var outputLabels = []string{"", "b ", "c "}
+var propLabels = []string{"mod 1", "mod 2", "mod 3", "mod 4", "mod 5"}
+var inputFields = []string{"input 1", "input 2", "input 3", "input 4", "input 5", "input 6", "input 7"}
+
 // LoadCubeRecipes populates CubeRecipes with the data from CubeMain.txt.
 func LoadCubeRecipes(file []byte) {
+
 	// Load data
 	d := d2common.LoadDataDictionary(string(file))
 
-	// Construct records
+	// Create records
 	CubeRecipes = make([]*CubeRecipeRecord, len(d.Data))
 	for idx := range d.Data {
 		CubeRecipes[idx] = &CubeRecipeRecord{
@@ -171,147 +123,61 @@ func LoadCubeRecipes(file []byte) {
 			MinDiff: d.GetNumber("min diff", idx),
 			Version: d.GetNumber("version", idx),
 
-			Requirement: CubeRecipeRequirement{
-				StatID:    d.GetNumber("param", idx),
-				Operation: d.GetNumber("op", idx),
-				Value:     d.GetNumber("value", idx),
-			},
+			StatID:    d.GetNumber("param", idx),
+			Operation: d.GetNumber("op", idx),
+			Value:     d.GetNumber("value", idx),
 
 			Class: classField(d.GetString("class", idx)),
 
 			NumInputs: d.GetNumber("numinputs", idx),
-
-			Inputs: inputFieldsArray(
-				d.GetString("input 1", idx),
-				d.GetString("input 2", idx),
-				d.GetString("input 3", idx),
-				d.GetString("input 4", idx),
-				d.GetString("input 5", idx),
-				d.GetString("input 6", idx),
-				d.GetString("input 7", idx),
-			),
-
-			//output
-			//lvl
-			//plvl
-			//ilvl
-			//mod 1
-			//mod 1 chance
-			//mod 1 param
-			//mod 1 min
-			//mod 1 max
-			//mod 2
-			//mod 2 chance
-			//mod 2 param
-			//mod 2 min
-			//mod 2 max
-			//mod 3
-			//mod 3 chance
-			//mod 3 param
-			//mod 3 min
-			//mod 3 max
-			//mod 4
-			//mod 4 chance
-			//mod 4 param
-			//mod 4 min
-			//mod 4 max
-			//mod 5
-			//mod 5 chance
-			//mod 5 param
-			//mod 5 min
-			//mod 5 max
-			//output b
-			//b lvl
-			//b plvl
-			//b ilvl
-			//b mod 1
-			//b mod 1 chance
-			//b mod 1 param
-			//b mod 1 min
-			//b mod 1 max
-			//b mod 2
-			//b mod 2 chance
-			//b mod 2 param
-			//b mod 2 min
-			//b mod 2 max
-			//b mod 3
-			//b mod 3 chance
-			//b mod 3 param
-			//b mod 3 min
-			//b mod 3 max
-			//b mod 4
-			//b mod 4 chance
-			//b mod 4 param
-			//b mod 4 min
-			//b mod 4 max
-			//b mod 5
-			//b mod 5 chance
-			//b mod 5 param
-			//b mod 5 min
-			//b mod 5 max
-			//output c
-			//c lvl
-			//c plvl
-			//c ilvl
-			//c mod 1
-			//c mod 1 chance
-			//c mod 1 param
-			//c mod 1 min
-			//c mod 1 max
-			//c mod 2
-			//c mod 2 chance
-			//c mod 2 param
-			//c mod 2 min
-			//c mod 2 max
-			//c mod 3
-			//c mod 3 chance
-			//c mod 3 param
-			//c mod 3 min
-			//c mod 3 max
-			//c mod 4
-			//c mod 4 chance
-			//c mod 4 param
-			//c mod 4 min
-			//c mod 4 max
-			//c mod 5
-			//c mod 5 chance
-			//c mod 5 param
-			//c mod 5 min
-			//c mod 5 max
-			//*eol
 		}
+
+		// Create inputs
+		CubeRecipes[idx].Inputs = make([]CubeRecipeItem, 7)
+		for i := range inputFields {
+			CubeRecipes[idx].Inputs[i] = newCubeRecipeItem(
+				d.GetString(inputFields[i], idx))
+		}
+
+		// Create outputs
+		CubeRecipes[idx].Outputs = make([]CubeRecipeResult, 3)
+		for o, outLabel := range outputLabels {
+
+			CubeRecipes[idx].Outputs[o] = CubeRecipeResult{
+				Item: newCubeRecipeItem(
+					d.GetString(outputFields[o], idx)),
+
+				Level:  d.GetNumber(outLabel+"lvl", idx),
+				ILevel: d.GetNumber(outLabel+"plvl", idx),
+				PLevel: d.GetNumber(outLabel+"ilvl", idx),
+			}
+
+			// Create properties
+			properties := make([]CubeRecipeItemProperty, 5)
+			for p, prop := range propLabels {
+
+				properties[p] = CubeRecipeItemProperty{
+					Code:   d.GetString(outLabel+prop, idx),
+					Chance: d.GetNumber(outLabel+prop+" chance", idx),
+					Param:  d.GetNumber(outLabel+prop+" param", idx),
+					Min:    d.GetNumber(outLabel+prop+" min", idx),
+					Max:    d.GetNumber(outLabel+prop+" max", idx),
+				}
+			}
+
+			CubeRecipes[idx].Outputs[o].Properties = properties
+		}
+
 	}
 
 	log.Printf( /*"Loaded %d CubeMainRecord records"*/ "LoadCubeRecipes ran - %d", len(CubeRecipes))
 }
 
-// inputFieldsArray constructs an array of CubeRecipeInput from an array of strings, ignoring empty strings.
-func inputFieldsArray(fields ...string) []CubeRecipeInput {
-	// Count fields with values
-	count := 0
-	for _, f := range fields {
-		if f != "" {
-			count++
-		}
-	}
-
-	// Construct CubeRecipeInputs
-	inputs := make([]CubeRecipeInput, count)
-	index := 0
-	for _, f := range fields {
-		if f != "" {
-			inputs[index] = newCubeRecipeInput(f)
-			index++
-		}
-	}
-	return inputs
-}
-
-// newCubeRecipe constructs a CubeRecipeInput from a string of arguments.
-func newCubeRecipeInput(f string) CubeRecipeInput {
+// newCubeRecipeItem constructs a CubeRecipeItem from a string of arguments. arguments include at least an item and sometimes parameters and/or a count (qty parameter).
+func newCubeRecipeItem(f string) CubeRecipeItem {
 	args := stringArray(f)
 
-	cube := CubeRecipeInput{
+	item := CubeRecipeItem{
 		Code:  args[0], // the first argument is always the item count
 		Count: 1,       // default to a count of 1 (no qty parameter)
 	}
@@ -319,36 +185,37 @@ func newCubeRecipeInput(f string) CubeRecipeInput {
 	// Ignore the first argument
 	args = args[1:]
 
-	// Find the qty parameter if it was provided, convert to int and assign to cube.Count
+	// Find the qty parameter if it was provided, convert to int and assign to item.Count
 	for idx, arg := range args {
 		if strings.HasPrefix(arg, "qty") {
 			count, err := strconv.Atoi(strings.Split(arg, "=")[1])
 			if err != nil {
 				log.Fatal("Error parsing item count:", err)
 			}
-			cube.Count = count
+			item.Count = count
 
-			// If qty isn't the last item, move the last item to its place
+			// Remove the qty parameter
 			if idx != len(args)-1 {
 				args[idx] = args[len(args)-1]
 			}
-			// Ignore the last item
 			args = args[:len(args)-1]
+
+			break
 		}
 	}
 
 	// No other arguments were provided
 	if len(args) == 0 {
-		return cube
+		return item
 	}
 
 	// Record the parameter strings
-	cube.Params = make([]string, len(args))
+	item.Params = make([]string, len(args))
 	for idx, arg := range args {
-		cube.Params[idx] = arg
+		item.Params[idx] = arg
 	}
 
-	return cube
+	return item
 }
 
 // classField converts class tokens to s2enum.Hero.
