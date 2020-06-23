e5898f74734713fde5fa4c056453ee15e194ce52
Jonathan Rosenboim <masterfqf@gmail.com>
2020-06-23 21:49:50 +0300

    Implement InventoryItem interface for InventoryItemMisc

--- d2core/d2inventory/inventory_item_misc.go
+++ d2core/d2inventory/inventory_item_misc.go
@@ -2,6 +2,7 @@ package d2inventory
 
 import (
 	"github.com/OpenDiablo2/OpenDiablo2/d2common/d2data/d2datadict"
+	"github.com/OpenDiablo2/OpenDiablo2/d2common/d2enum"
 	"log"
 )
 
@@ -25,4 +26,39 @@ func GetMiscItemByCode(code string) *InventoryItemMisc {
 		ItemName:       result.Name,
 		ItemCode:       result.Code,
 	}
+}
+
+func (v *InventoryItemMisc) InventoryItemName() string {
+	if v == nil {
+		return ""
+	}
+	return v.ItemName
+}
+
+func (v *InventoryItemMisc) InventoryItemType() d2enum.InventoryItemType {
+	return d2enum.InventoryItemTypeItem
+}
+
+func (v *InventoryItemMisc) InventoryGridSize() (int, int) {
+	return v.InventorySizeX, v.InventorySizeY
+}
+
+func (v *InventoryItemMisc) InventoryGridSlot() (int, int) {
+	return v.InventorySlotX, v.InventorySlotY
+}
+
+func (v *InventoryItemMisc) SetInventoryGridSlot(x int, y int) {
+	v.InventorySlotX, v.InventorySlotY = x, y
+}
+
+func (v *InventoryItemMisc) Serialize() []byte {
+	return []byte{}
+}
+
+func (v *InventoryItemMisc) GetItemCode() string {
+	if v == nil {
+		return ""
+	}
+
+	return v.ItemCode
 }
