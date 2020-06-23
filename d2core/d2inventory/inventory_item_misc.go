6a0febd5a76385dc29ba9726b8990c7e0e332837
Jonathan Rosenboim <masterfqf@gmail.com>
2020-06-23 21:44:53 +0300

    Add GetMiscItemByCode function

--- d2core/d2inventory/inventory_item_misc.go
+++ d2core/d2inventory/inventory_item_misc.go
@@ -1,5 +1,10 @@
 package d2inventory
 
+import (
+	"github.com/OpenDiablo2/OpenDiablo2/d2common/d2data/d2datadict"
+	"log"
+)
+
 type InventoryItemMisc struct {
 	InventorySizeX int    `json:"inventorySizeX"`
 	InventorySizeY int    `json:"inventorySizeY"`
@@ -7,4 +12,17 @@ type InventoryItemMisc struct {
 	InventorySlotY int    `json:"inventorySlotY"`
 	ItemName       string `json:"itemName"`
 	ItemCode       string `json:"itemCode"`
+}
+
+func GetMiscItemByCode(code string) *InventoryItemMisc {
+	result := d2datadict.MiscItems[code]
+	if result == nil {
+		log.Fatalf("Could not find misc item entry for code '%s'", code)
+	}
+	return &InventoryItemMisc{
+		InventorySizeX: result.InventoryWidth,
+		InventorySizeY: result.InventoryHeight,
+		ItemName:       result.Name,
+		ItemCode:       result.Code,
+	}
 }
