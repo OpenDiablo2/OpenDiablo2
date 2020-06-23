d5d8487a0477b489fc99746e10ef0342a82b530b
Jonathan Rosenboim <masterfqf@gmail.com>
2020-06-23 20:07:44 +0300

    Change comment to start with capital I

--- d2common/d2enum/inventory_item_type.go
+++ d2common/d2enum/inventory_item_type.go
@@ -3,7 +3,7 @@ package d2enum
 type InventoryItemType int
 
 const (
-	InventoryItemTypeItem   InventoryItemType = 0 // item
+	InventoryItemTypeItem   InventoryItemType = 0 // Item
 	InventoryItemTypeWeapon InventoryItemType = 1 // Weapon
 	InventoryItemTypeArmor  InventoryItemType = 2 // Armor
 )
