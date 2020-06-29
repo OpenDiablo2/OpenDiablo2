06202a2ddf2531915c018b32b89203b0502d97d6
dk <dknuth0101@gmail.com>
2020-06-29 15:46:31 -0700

    fixed monstats lint errors, added doc file for d2datadict (#494)
    * adding comments to d2interface for linter
    
    * moved d2render renderer interfaces and types into d2interface
    
    * fixed most lint errors for monstats loader

---
+++ d2common/d2data/d2datadict/d2datadict.go
@@ -0,0 +1,3 @@
+// Package d2datadict parses txt files as data dictionaries and exports records arrays
+// For the Diablo II MPQ's, data dictionaries are the tab-separated value txt files found in `data/global/excel`
+package d2datadict
