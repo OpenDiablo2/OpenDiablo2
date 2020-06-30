ad660fb9822f644d1763e1b087614cf8059ef23b
gurkankaymak <gurkankaymak@gmail.com>
2020-06-30 11:55:40 +0300

    fixed lint issues of the package d2core/d2inventory

--- d2core/d2inventory/inventory_item_weapon.go
+++ d2core/d2inventory/inventory_item_weapon.go
@@ -7,6 +7,7 @@ import (
 	"github.com/OpenDiablo2/OpenDiablo2/d2common/d2enum"
 )
 
+// InventoryItemWeapon stores the info of an weapon item in the inventory
 type InventoryItemWeapon struct {
 	InventorySizeX     int    `json:"inventorySizeX"`
 	InventorySizeY     int    `json:"inventorySizeY"`
@@ -18,12 +19,14 @@ type InventoryItemWeapon struct {
 	WeaponClassOffHand string `json:"weaponClassOffHand"`
 }
 
+// GetWeaponItemByCode returns the weapon item for the given code
 func GetWeaponItemByCode(code string) *InventoryItemWeapon {
 	// TODO: Non-normal codes will fail here...
 	result := d2datadict.Weapons[code]
 	if result == nil {
 		log.Fatalf("Could not find weapon entry for code '%s'", code)
 	}
+
 	return &InventoryItemWeapon{
 		InventorySizeX:     result.InventoryWidth,
 		InventorySizeY:     result.InventoryHeight,
@@ -34,50 +37,63 @@ func GetWeaponItemByCode(code string) *InventoryItemWeapon {
 	}
 }
 
+// GetWeaponClass returns the class of the weapon
 func (v *InventoryItemWeapon) GetWeaponClass() string {
 	if v == nil || v.ItemCode == "" {
 		return "hth"
 	}
+
 	return v.WeaponClass
 }
 
+// GetWeaponClassOffHand returns the class of the off hand weapon
 func (v *InventoryItemWeapon) GetWeaponClassOffHand() string {
 	if v == nil || v.ItemCode == "" {
 		return ""
 	}
+
 	return v.WeaponClassOffHand
 }
 
+// InventoryItemName returns the name of the weapon
 func (v *InventoryItemWeapon) InventoryItemName() string {
 	if v == nil {
 		return ""
 	}
+
 	return v.ItemName
 }
 
+// InventoryItemType returns the item type of the weapon
 func (v *InventoryItemWeapon) InventoryItemType() d2enum.InventoryItemType {
 	return d2enum.InventoryItemTypeWeapon
 }
 
-func (v *InventoryItemWeapon) InventoryGridSize() (int, int) {
+// InventoryGridSize returns the grid size of the weapon
+func (v *InventoryItemWeapon) InventoryGridSize() (sizeX, sizeY int) {
 	return v.InventorySizeX, v.InventorySizeY
 }
 
-func (v *InventoryItemWeapon) InventoryGridSlot() (int, int) {
+// InventoryGridSlot returns the grid slot coordinates of the weapon
+func (v *InventoryItemWeapon) InventoryGridSlot() (slotX, slotY int) {
 	return v.InventorySlotX, v.InventorySlotY
 }
 
-func (v *InventoryItemWeapon) SetInventoryGridSlot(x int, y int) {
+// SetInventoryGridSlot sets the InventorySlotX and InventorySlotY of the weapon with the given x and y values
+func (v *InventoryItemWeapon) SetInventoryGridSlot(x, y int) {
 	v.InventorySlotX, v.InventorySlotY = x, y
 }
 
+// Serialize returns the weapon object as a byte array
 func (v *InventoryItemWeapon) Serialize() []byte {
 	return []byte{}
 }
 
+// GetItemCode returns the item code of the weapon
 func (v *InventoryItemWeapon) GetItemCode() string {
 	if v == nil {
 		return ""
 	}
+
 	return v.ItemCode
 }
