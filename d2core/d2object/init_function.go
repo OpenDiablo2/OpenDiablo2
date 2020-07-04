094730027cffc15fd9853449c144b70061ca1fac
Ziemas <ziemas@ziemas.se>
2020-07-04 05:51:31 +0200

    Turn on waypoints

--- d2core/d2object/init_function.go
+++ d2core/d2object/init_function.go
@@ -9,6 +9,7 @@ func initObject(ob *Object) bool {
 	funcs := map[int]func(*Object){
 		8:  initTorch,
 		14: initTorch,
+		17: initWaypoint,
 	}
 
 	fun, ok := funcs[ob.objectRecord.InitFn]
@@ -23,6 +24,13 @@ func initObject(ob *Object) bool {
 
 // Initializes torch/brazier type objects
 func initTorch(ob *Object) {
+	if ob.objectRecord.HasAnimationMode[d2enum.AnimationModeObjectOperating] {
+		ob.setMode("ON", 0)
+	}
+}
+
+func initWaypoint(ob *Object) {
+	// Turn these on unconditionally for now, they look nice :)
 	if ob.objectRecord.HasAnimationMode[d2enum.AnimationModeObjectOperating] {
 		ob.setMode("ON", 0)
 	}
