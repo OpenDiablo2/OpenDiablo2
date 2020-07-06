ad0986642e64e83c5b2c0ac4dc0f01a2891c1135
danhale-git <danhale87@gmail.com>
2020-07-06 18:50:24 +0100

    CompareFloat64Fuzzy delta direction reversed.

--- d2common/d2math/math.go
+++ d2common/d2math/math.go
@@ -11,12 +11,12 @@ const (
 // the comparison of floats a and b. 0 will be returned if the
 // absolute difference between a and b is less than epsilon.
 func CompareFloat64Fuzzy(a, b *float64) int {
-	difference := *a - *b
-	if math.Abs(difference) < epsilon {
+	delta := *b - *a
+	if math.Abs(delta) < epsilon {
 		return 0
 	}
 
-	if difference > 0 {
+	if delta > 0 {
 		return 1
 	}
 
