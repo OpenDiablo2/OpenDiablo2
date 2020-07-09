76ac0c9f247e765851774fe20c3a9f85bf4e7528
danhale-git <danhale87@gmail.com>
2020-07-09 07:50:19 +0100

    Panic on NaN and Inf in Position.

--- d2common/d2math/d2vector/position.go
+++ d2common/d2math/d2vector/position.go
@@ -1,56 +1,68 @@
 package d2vector
 
-// Position is a vector in world space. The stored
-// value is  the one returned by Position.World()
+import (
+	"fmt"
+	"math"
+)
+
+// Position is a vector in world space. The stored value is  the one returned by Position.World()
 type Position struct {
 	Vector
 }
 
-// NewPosition creates a new Position at the given
-// float64 world position.
+// NewPosition creates a new Position at the given float64 world position.
 func NewPosition(x, y float64) *Position {
-	// TODO: BigFloat dependency
-	return &Position{NewVector(x, y)}
+	p := &Position{NewVector(x, y)}
+	p.checkValues()
+	return p
 }
 
-// World is the position, where 1 = one map
-// tile.
+// Set sets this position to the given x and y values.
+func (p *Position) Set(x, y float64) {
+	p.x, p.y = x, y
+	p.checkValues()
+}
+
+func (p *Position) checkValues() {
+	if math.IsNaN(p.x) || math.IsNaN(p.y) {
+		panic(fmt.Sprintf("float value is NaN: %s", p.Vector))
+	}
+
+	if math.IsInf(p.x, 0) || math.IsInf(p.y, 0) {
+		panic(fmt.Sprintf("float value is Inf: %s", p.Vector))
+	}
+}
+
+// World is the position, where 1 = one map tile.
 func (p *Position) World() *Vector {
 	return &p.Vector
 }
 
-// Tile is the tile position, always a whole
-// number.
+// Tile is the tile position, always a whole number.
 func (p *Position) Tile() *Vector {
 	c := p.World().Clone()
 	return c.Floor()
 }
 
-// TileOffset is the offset from the tile position,
-// always < 1.
+// TileOffset is the offset from the tile position, always < 1.
 func (p *Position) TileOffset() *Vector {
 	c := p.World().Clone()
 	return c.Subtract(p.Tile())
 }
 
-// SubWorld is the position, where 5 = one map
-// tile.
+// SubWorld is the position, where 5 = one map tile.
 func (p *Position) SubWorld() *Vector {
 	c := p.World().Clone()
-	// TODO: BigFloat dependency
 	return c.Scale(5)
 }
 
-// SubTile is the tile position in sub tiles,
-// always a multiple of 5.
+// SubTile is the tile position in sub tiles, always a multiple of 5.
 func (p *Position) SubTile() *Vector {
 	c := p.Tile().Clone()
-	// TODO: BigFloat dependency
 	return c.Scale(5)
 }
 
-// SubTileOffset is the offset from the sub tile
-// position in sub tiles, always < 1.
+// SubTileOffset is the offset from the sub tile position in sub tiles, always < 1.
 func (p *Position) SubTileOffset() *Vector {
 	c := p.SubWorld().Clone()
 	return c.Subtract(p.SubTile())
