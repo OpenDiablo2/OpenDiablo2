53176d727e75139574f32cd906dd73c1a9530260
dknuth <dknuth0101@gmail.com>
2020-07-30 03:00:46 -0700

    adding equipper interface as part of stat context, eventually to be used to resolve set bonus (among other things)

--- d2core/d2item/context.go
+++ d2core/d2item/context.go
@@ -6,6 +6,7 @@ import "github.com/OpenDiablo2/OpenDiablo2/d2core/d2stats"
 // This is used for resolving stat dependencies for showing actual values, like
 // stats that are based off of the current character level
 type StatContext interface {
+	Equipper
 	BaseStatList() d2stats.StatList
 	StatList() d2stats.StatList
 }
