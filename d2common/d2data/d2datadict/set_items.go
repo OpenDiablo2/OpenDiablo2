70ae9b1e3509de2a7f93c7f8b785cca8bb59e770
dknuth <dknuth0101@gmail.com>
2020-07-30 02:57:57 -0700

    changed set items records export to a map of their codes to the records, grouped property params into a struct

--- d2common/d2data/d2datadict/set_items.go
+++ d2common/d2data/d2datadict/set_items.go
@@ -1,15 +1,31 @@
 package d2datadict
 
 import (
+	"fmt"
 	"github.com/OpenDiablo2/OpenDiablo2/d2common"
 	"log"
 )
 
+const (
+	numPropertiesOnSetItem = 9
+	numBonusPropertiesOnSetItem = 5
+	bonusToken1 = "a"
+	bonusToken2 = "b"
+	propCodeFmt = "prop%d"
+	propParamFmt = "par%d"
+	propMinFmt = "min%d"
+	propMaxFmt = "max%d"
+	bonusCodeFmt = "aprop%d%s"
+	bonusParamFmt = "apar%d%s"
+	bonusMinFmt = "amin%d%s"
+	bonusMaxFmt = "amax%d%s"
+)
+
 // SetItemRecord represents a set item
 type SetItemRecord struct {
-	// StringTableKey (index)
+	// SetItemKey (index)
 	// string key to item's name in a .tbl file
-	StringTableKey string
+	SetItemKey string
 
 	// SetKey (set)
 	// string key to the index field in Sets.txt - the set the item is a part of.
@@ -90,63 +106,37 @@ type SetItemRecord struct {
 	// on a set item. See the appendix for further details about this field's effects.
 	AddFn int
 
-	// Prop (prop1 to prop9)
-	// An ID pointer of a property from Properties.txt,
-	// these columns control each of the nine different fixed (
-	// blue) modifiers a set item can grant you at most.
-	Prop [9]string
+	// Properties are a propert code, parameter, min, max for generating an item propert
+	Properties [numPropertiesOnSetItem]*SetItemProperty
 
-	// Par (par1 to par9)
-	// The parameter passed on to the associated property, this is used to pass skill IDs, state IDs,
-	// monster IDs, montype IDs and the like on to the properties that require them,
-	// these fields support calculations.
-	Par [9]int
+	// SetPropertiesLevel1 is the first version of bonus properties for the set
+	SetPropertiesLevel1 [numBonusPropertiesOnSetItem]*SetItemProperty
 
-	// Min, Max (min1 to min9, max1 to max9)
-	// Minimum value to assign to the associated (blue) property.
-	// Certain properties have special interpretations based on stat encoding (e.g.
-	// chance-to-cast and charged skills). See the File Guide for Properties.txt and ItemStatCost.
-	// txt for further details.
-	Min [9]int
-	Max [9]int
+	// SetPropertiesLevel2 is the second version of bonus properties for the set
+	SetPropertiesLevel2 [numBonusPropertiesOnSetItem]*SetItemProperty
+}
 
-	// APropA, APropB (aprop1a,aprop1b to aprop5a,aprop5b)
-	// An ID pointer of a property from Properties.txt,
-	// these columns control each of the five pairs of different variable (
-	// green) modifiers a set item can grant you at most.
-	APropA [5]string
-	APropB [5]string
-
-	// AParA, AParB (apar1a,apar1b to apar5a,apar5b)
-	// The parameter passed on to the associated property, this is used to pass skill IDs, state IDs,
-	// monster IDs, montype IDs and the like on to the properties that require them,
-	// these fields support calculations.
-	AParA [5]int
-	AParB [5]int
-
-	// AMinA, AMinB, AMaxA, AMaxB (amin1a,amin1b to amin5a,amin5b)
-	// Minimum value to assign to the associated property.
-	// Certain properties have special interpretations based on stat encoding (e.g.
-	// chance-to-cast and charged skills). See the File Guide for Properties.txt and ItemStatCost.
-	// txt for further details.
-	AMinA [5]int
-	AMinB [5]int
-	AMaxA [5]int
-	AMaxB [5]int
+// SetItemProperty is describes a property of a set item
+type SetItemProperty struct {
+	Code      string
+	Parameter string // depending on the property, this may be an int (usually), or a string
+	Min       int
+	Max       int
 }
 
 // SetItems holds all of the SetItemRecords
-var SetItems []*SetItemRecord //nolint:gochecknoglobals // Currently global by design, only written once
+var SetItems map[string]*SetItemRecord //nolint:gochecknoglobals // Currently global by design,
+// only written once
 
 // LoadSetItems loads all of the SetItemRecords from SetItems.txt
 func LoadSetItems(file []byte) {
-	SetItems = make([]*SetItemRecord, 0)
+	SetItems = make(map[string]*SetItemRecord)
 
 	d := d2common.LoadDataDictionary(file)
 
 	for d.Next() {
 		record := &SetItemRecord{
-			StringTableKey:            d.String("index"),
+			SetItemKey:                d.String("index"),
 			SetKey:                    d.String("set"),
 			ItemCode:                  d.String("item"),
 			Rarity:                    d.Number("rarity"),
@@ -162,109 +152,47 @@ func LoadSetItems(file []byte) {
 			CostMult:                  d.Number("cost mult"),
 			CostAdd:                   d.Number("cost add"),
 			AddFn:                     d.Number("add func"),
-			Prop: [9]string{
-				d.String("prop1"),
-				d.String("prop2"),
-				d.String("prop3"),
-				d.String("prop4"),
-				d.String("prop5"),
-				d.String("prop6"),
-				d.String("prop7"),
-				d.String("prop8"),
-				d.String("prop9"),
-			},
-			Par: [9]int{
-				d.Number("par1"),
-				d.Number("par2"),
-				d.Number("par3"),
-				d.Number("par4"),
-				d.Number("par5"),
-				d.Number("par6"),
-				d.Number("par7"),
-				d.Number("par8"),
-				d.Number("par9"),
-			},
-			Min: [9]int{
-				d.Number("min1"),
-				d.Number("min2"),
-				d.Number("min3"),
-				d.Number("min4"),
-				d.Number("min5"),
-				d.Number("min6"),
-				d.Number("min7"),
-				d.Number("min8"),
-				d.Number("min9"),
-			},
-			Max: [9]int{
-				d.Number("max1"),
-				d.Number("max2"),
-				d.Number("max3"),
-				d.Number("max4"),
-				d.Number("max5"),
-				d.Number("max6"),
-				d.Number("max7"),
-				d.Number("max8"),
-				d.Number("max9"),
-			},
-			APropA: [5]string{
-				d.String("aprop1a"),
-				d.String("aprop2a"),
-				d.String("aprop3a"),
-				d.String("aprop4a"),
-				d.String("aprop5a"),
-			},
-			APropB: [5]string{
-				d.String("aprop1b"),
-				d.String("aprop2b"),
-				d.String("aprop3b"),
-				d.String("aprop4b"),
-				d.String("aprop5b"),
-			},
-			AParA: [5]int{
-				d.Number("apar1a"),
-				d.Number("apar2a"),
-				d.Number("apar3a"),
-				d.Number("apar4a"),
-				d.Number("apar5a"),
-			},
-			AParB: [5]int{
-				d.Number("apar1b"),
-				d.Number("apar2b"),
-				d.Number("apar3b"),
-				d.Number("apar4b"),
-				d.Number("apar5b"),
-			},
-			AMinA: [5]int{
-				d.Number("amin1a"),
-				d.Number("amin2a"),
-				d.Number("amin3a"),
-				d.Number("amin4a"),
-				d.Number("amin5a"),
-			},
-			AMinB: [5]int{
-				d.Number("amin1b"),
-				d.Number("amin2b"),
-				d.Number("amin3b"),
-				d.Number("amin4b"),
-				d.Number("amin5b"),
-			},
-			AMaxA: [5]int{
-				d.Number("amax1a"),
-				d.Number("amax2a"),
-				d.Number("amax3a"),
-				d.Number("amax4a"),
-				d.Number("amax5a"),
-			},
-			AMaxB: [5]int{
-				d.Number("amax1b"),
-				d.Number("amax2b"),
-				d.Number("amax3b"),
-				d.Number("amax4b"),
-				d.Number("amax5b"),
-			},
+
 		}
 
-		SetItems = append(SetItems, record)
+		// normal properties
+		props := [numPropertiesOnSetItem]*SetItemProperty{}
+
+		for idx := 0; idx < numPropertiesOnSetItem; idx++ {
+			num := idx + 1
+			props[idx] = &SetItemProperty{
+				d.String(fmt.Sprintf(propCodeFmt, num)),
+				d.String(fmt.Sprintf(propParamFmt, num)),
+				d.Number(fmt.Sprintf(propMinFmt, num)),
+				d.Number(fmt.Sprintf(propMaxFmt, num)),
+			}
+		}
+
+		// set bonus properties
+		bonus1 := [numBonusPropertiesOnSetItem]*SetItemProperty{}
+		bonus2 := [numBonusPropertiesOnSetItem]*SetItemProperty{}
+
+		for idx := 0; idx < numBonusPropertiesOnSetItem; idx++ {
+			num := idx + 1
+
+			bonus1[idx] = &SetItemProperty{
+				d.String(fmt.Sprintf(bonusCodeFmt, num, bonusToken1)),
+				d.String(fmt.Sprintf(bonusParamFmt, num, bonusToken1)),
+				d.Number(fmt.Sprintf(bonusMinFmt, num,bonusToken1)),
+				d.Number(fmt.Sprintf(bonusMaxFmt, num, bonusToken1)),
+			}
+
+			bonus2[idx] = &SetItemProperty{
+				d.String(fmt.Sprintf(bonusCodeFmt, num, bonusToken2)),
+				d.String(fmt.Sprintf(bonusParamFmt, num, bonusToken2)),
+				d.Number(fmt.Sprintf(bonusMinFmt, num,bonusToken2)),
+				d.Number(fmt.Sprintf(bonusMaxFmt, num, bonusToken2)),
+			}
+		}
+
+		record.Properties = props
+
+		SetItems[record.SetItemKey] = record
 	}
 
 	if d.Err != nil {
