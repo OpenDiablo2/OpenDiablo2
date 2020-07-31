06c46b7c906611af42d0f31873d29bebd3f09663
Ziemas <ziemas@ziemas.se>
2020-07-31 03:40:56 +0200

    Stereo panning for sound effects
    Randomly pan event sounds
    
    Adapted from ebiten example

--- d2core/d2audio/sound_environment.go
+++ d2core/d2audio/sound_environment.go
@@ -1,6 +1,8 @@
 package d2audio
 
 import (
+	"math/rand"
+
 	"github.com/OpenDiablo2/OpenDiablo2/d2common/d2data/d2datadict"
 )
 
@@ -50,6 +52,11 @@ func (s *SoundEnvironment) Advance(elapsed float64) {
 	s.eventTimer -= elapsed
 	if s.eventTimer < 0 {
 		s.eventTimer = float64(s.environment.EventDelay) / 25
-		s.engine.PlaySoundID(s.environment.DayEvent)
+
+		snd := s.engine.PlaySoundID(s.environment.DayEvent)
+		if snd != nil {
+			pan := (rand.Float64() * 2) - 1
+			snd.SetPan(pan)
+		}
 	}
 }
