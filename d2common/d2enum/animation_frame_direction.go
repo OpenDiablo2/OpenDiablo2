6d521ff92ed051fa9fce940e7fdfd34fbfdd886d
dknuth <dknuth0101@gmail.com>
2020-08-06 09:44:54 -0700

    fixed lint errors in d2enum

--- d2common/d2enum/animation_frame_direction.go
+++ d2common/d2enum/animation_frame_direction.go
@@ -3,6 +3,7 @@ package d2enum
 // AnimationFrameDirection enumerates animation frame directions used in d2datadict.MonsterSequenceFrame
 type AnimationFrameDirection int
 
+// Animation frame directions
 const (
 	SouthWest AnimationFrameDirection = iota
 	NorthWest
--- d2common/d2enum/pet_icon_type.go
+++ d2common/d2enum/pet_icon_type.go
@@ -4,10 +4,10 @@ package d2enum
 type PetIconType int
 
 // Pet icon types
-//The information has been gathered from [https://d2mods.info/forum/kb/viewarticle?a=355]
+// The information has been gathered from [https:// d2mods.info/forum/kb/viewarticle?a=355]
 const (
 	NoIcon PetIconType = iota
 	ShowIconOnly
-	ShowIconAndQuantity //Quantity, such as the number of skeletons
+	ShowIconAndQuantity // Quantity, such as the number of skeletons
 	ShowIconOnly2
 )
