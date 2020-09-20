ea194f87981d7e775ba484b84e4cc041f523d72a
dknuth <dknuth0101@gmail.com>
2020-09-20 16:56:23 -0700

    remoed Shrines singleton from d2datadict

--- d2app/app.go
+++ d2app/app.go
@@ -227,7 +227,6 @@ func (a *App) loadDataDict() error {
 		{d2resource.AnimationData, d2data.LoadAnimationData},
 		{d2resource.SuperUniques, d2datadict.LoadSuperUniques},
 		{d2resource.TreasureClass, d2datadict.LoadTreasureClassRecords},
-		{d2resource.Shrines, d2datadict.LoadShrines},
 		{d2resource.UniqueAppellation, d2datadict.LoadUniqueAppellations},
 	}
 
