0d0fdb53c68fd1624dc3ec56b17fefb016c8c3fb
Nicholas Eden <nicholas.eden@gmail.com>
2020-09-22 21:25:58 -0700

    Add mpq extract util to repo
    The gist with this script disappeared, adding to the repo since it's so useful.
    Added some error handling/logging to the script.

---
+++ utils/extract-mpq/doc.go
@@ -0,0 +1,13 @@
+// This command line utility provides a way to extract mpq files.
+//
+// Flags:
+// -o [directory] Output directory
+// -v Enable verbose output
+//
+// Usage:
+// First run `go install extract-mpq.go` in this directory.
+// Navigate to the Diablo II directory (ex: C:/Program Files (x86)/Diablo II)
+// then run extract-mpq(.exe) with the filename of the mpq to be extracted.
+//
+// extract-mpq d2char.mpq
+package main
