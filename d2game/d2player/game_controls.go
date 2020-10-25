1ab24b7d42f10756be9a30e882d63912d367720d
juander <juander@rumtueddeln.de>
2020-10-25 15:46:45 +0100

    d2player/game_controls: Fix wrong position of left/right menu
    the character would not detect that a click was in a menu and start
    walking.

--- d2game/d2player/game_controls.go
+++ d2game/d2player/game_controls.go
@@ -221,12 +221,12 @@ const (
 	menuLeftRectX,
 	menuLeftRectY,
 	menuLeftRectW,
-	menuLeftRectH = 0, 550, 800, 50
+	menuLeftRectH = 0, 0, 400, 600
 
 	menuRightRectX,
 	menuRightRectY,
 	menuRightRectW,
-	menuRightRectH = 0, 550, 800, 50
+	menuRightRectH = 400, 0, 400, 600
 )
 
 // GameControls represents the game's controls on the screen
