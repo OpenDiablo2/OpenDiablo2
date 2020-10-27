2d8f7ea3997b29dda047e530a1e72cbdd9fbaaad
Henry Asbridge <henry@vorax.org>
2020-10-27 18:02:10 +0000

    Made it less shouty, and added channel

--- main.go
+++ main.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"strconv"
-	"time"
 
 	"github.com/OpenDiablo2/OpenDiablo2/d2app"
-	"github.com/OpenDiablo2/OpenDiablo2/d2core/d2render/ebiten"
-	"github.com/OpenDiablo2/OpenDiablo2/d2networking/d2server"
-
 	"github.com/OpenDiablo2/OpenDiablo2/d2core/d2asset"
 	ebiten2 "github.com/OpenDiablo2/OpenDiablo2/d2core/d2audio/ebiten"
 	"github.com/OpenDiablo2/OpenDiablo2/d2core/d2config"
 	"github.com/OpenDiablo2/OpenDiablo2/d2core/d2input"
+	"github.com/OpenDiablo2/OpenDiablo2/d2core/d2render/ebiten"
 	"github.com/OpenDiablo2/OpenDiablo2/d2core/d2term"
+	"github.com/OpenDiablo2/OpenDiablo2/d2networking/d2server"
 	"github.com/OpenDiablo2/OpenDiablo2/d2script"
 )
 
@@ -33,6 +30,43 @@ func main() {
 
 	if err := d2config.Load(); err != nil {
 		panic(err)
+	}
+
+	var listen bool
+
+	var maxplayers int = 3
+
+	for argCount, arg := range os.Args {
+		if arg == "-listen" {
+			listen = true
+		}
+
+		if arg == "-maxplayers" {
+			max, _ := strconv.ParseInt(os.Args[argCount+1], 10, 32)
+			maxplayers = int(max)
+		}
+	}
+
+	if listen {
+		srvAsset, err := d2asset.NewAssetManager(d2config.Config)
+		if err != nil {
+			panic(err)
+		}
+
+		server, _ := d2server.NewGameServer(srvAsset, true, maxplayers)
+		serverErr := server.Start()
+		serverChannel := make(chan string)
+
+		if serverErr != nil {
+			log.Fatal(serverErr)
+		}
+
+		for {
+			message := <-serverChannel // this is blocking
+			if message == "stop" {
+				break
+			}
+		}
 	}
 
 	// NewAssetManager our providers
@@ -64,37 +98,6 @@ func main() {
 	}
 
 	scriptEngine := d2script.CreateScriptEngine()
-
-	var listen bool
-
-	var maxplayers int = 3
-
-	for argCount, arg := range os.Args {
-		if arg == "-listen" {
-			listen = true
-		}
-
-		if arg == "-maxplayers" {
-			max, _ := strconv.ParseInt(os.Args[argCount+1], 10, 32)
-			maxplayers = int(max)
-		}
-	}
-
-	if listen {
-		server, _ := d2server.NewGameServer(asset, true, maxplayers)
-		serverErr := server.Start()
-
-		if serverErr != nil {
-			log.Fatal(serverErr)
-			os.Exit(1)
-		}
-
-		for {
-			fmt.Println("Ah yes, another second of being a server, lovely")
-			// This is jank to get the goroutine of server.Start to continue
-			time.Sleep(time.Second)
-		}
-	}
 
 	app := d2app.Create(GitBranch, GitCommit, inputManager, term, scriptEngine, audio, renderer, asset)
 	if err := app.Run(); err != nil {
