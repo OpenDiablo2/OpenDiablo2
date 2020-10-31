c3c48c8497db755262db5a6203e2a7dd71ec0d76
Stephen Horan <steve.horan@theatsgroup.com>
2020-10-31 14:31:44 -0400

    Making suggested edits to move to an empty packet

--- d2networking/d2netpacket/packet_server_full.go
+++ d2networking/d2netpacket/packet_server_full.go
@@ -3,23 +3,17 @@ package d2netpacket
 import (
 	"encoding/json"
 	"log"
-	"time"
 
 	"github.com/OpenDiablo2/OpenDiablo2/d2networking/d2netpacket/d2netpackettype"
 )
 
-// ServerFullPacket contains the current time. It is sent by the server
-// to inform a client that the server has reached the max number of allowed connections.
-type ServerFullPacket struct {
-	TS time.Time `json:"ts"`
-}
+// ServerFullPacket is sent by the server to inform a client that the
+// server has reached the max number of allowed connections.
+type ServerFullPacket struct{}
 
-// CreateServerFullPacket returns a NetPacket which declares a
-// ServerFullPacket with the current time.
+// CreateServerFullPacket returns a NetPacket which declares a ServerFullPacket.
 func CreateServerFullPacket() NetPacket {
-	serverClosed := ServerFullPacket{
-		TS: time.Now(),
-	}
+	serverClosed := ServerFullPacket{}
 
 	b, err := json.Marshal(serverClosed)
 	if err != nil {
