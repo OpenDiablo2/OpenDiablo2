6d57dad60b889d706e1cc62f69a2475dd7abf4ee
M. Sz <mszeptuch@protonmail.com>
2020-11-05 11:45:51 +0100

    Shortcuts in Main Menu

--- d2game/d2gamescreen/main_menu.go
+++ d2game/d2gamescreen/main_menu.go
@@ -493,36 +493,37 @@ func (v *MainMenu) OnMouseButtonDown(event d2interface.MouseEvent) bool {
 	return false
 }
 
+
 func (v *MainMenu) EscapePressed(event d2interface.KeyEvent, mode mainMenuScreenMode) {
-	if event.Key() == d2enum.KeyEscape {
-		v.SetScreenMode(mode)
-	}
+        if event.Key() == d2enum.KeyEscape {
+                v.SetScreenMode(mode)
+        }
 }
 
 // OnKeyUp is called when a key is released
 func (v *MainMenu) OnKeyUp(event d2interface.KeyEvent) bool {
-	switch v.screenMode {
-	case ScreenModeTrademark: // On retail version of D2, some specific key events (Escape, Space and Enter) puts you onto the main menu.
-		switch event.Key() {
-		case d2enum.KeyEscape, d2enum.KeyEnter, d2enum.KeySpace:
-			v.SetScreenMode(ScreenModeMainMenu)
-		}
-		return true
-	case ScreenModeMainMenu: // pressing escape in Main Menu close the game
-		if event.Key() == d2enum.KeyEscape {
-			v.onExitButtonClicked()
-		}
-	case ScreenModeMultiplayer: // back to previous menu
-		v.EscapePressed(event, ScreenModeMainMenu)
-		return true
+        switch v.screenMode {
+        case ScreenModeTrademark: // On retail version of D2, some specific key events (Escape, Space and Enter) puts you onto the main menu.
+                switch event.Key() {
+                case d2enum.KeyEscape, d2enum.KeyEnter, d2enum.KeySpace:
+                        v.SetScreenMode(ScreenModeMainMenu)
+                }
+                return true
+        case ScreenModeMainMenu: // pressing escape in Main Menu close the game
+                if event.Key() == d2enum.KeyEscape {
+                        v.onExitButtonClicked()
+                }
+        case ScreenModeMultiplayer: // back to previous menu
+                v.EscapePressed(event, ScreenModeMainMenu)
+                return true
         case ScreenModeTCPIP: // back to previous menu
-		v.EscapePressed(event, ScreenModeMultiplayer)
-		return true
+                v.EscapePressed(event, ScreenModeMultiplayer)
+                return true
         case ScreenModeServerIP: // back to previous menu
-		v.EscapePressed(event, ScreenModeTCPIP)
-		return true
-	}
-	return false
+                v.EscapePressed(event, ScreenModeTCPIP)
+                return true
+        }
+        return false
 }
 
 // SetScreenMode sets the screen mode (which sub-menu the screen is on)
