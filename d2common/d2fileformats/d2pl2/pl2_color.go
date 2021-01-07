a375066b822b3dfb9932be45bebb66bbdf7fd750
gravestench <dknuth0101@gmail.com>
2021-01-06 16:53:09 -0800

    make pl2 color implement d2interface.color

--- d2common/d2fileformats/d2pl2/pl2_color.go
+++ d2common/d2fileformats/d2pl2/pl2_color.go
@@ -2,8 +2,47 @@ package d2pl2
 
 // PL2Color represents an RGBA color
 type PL2Color struct {
-	R uint8
-	G uint8
-	B uint8
+	r uint8
+	g uint8
+	b uint8
 	_ uint8
 }
+
+func (c PL2Color) R() uint8 {
+	return c.r
+}
+
+func (c PL2Color) G() uint8 {
+	return c.g
+}
+
+func (c PL2Color) B() uint8 {
+	return c.g
+}
+
+func (c PL2Color) A() uint8 {
+	return 0xff
+}
+
+func (c PL2Color) RGBA() uint32 {
+	return uint32(c.r)<<24 | uint32(c.g)<<16 | uint32(c.b)<<8 | uint32(0xff)
+}
+
+func (c PL2Color) SetRGBA(u uint32) {
+	c.r = byte((u>>24)&0xff)
+	c.g = byte((u>>16)&0xff)
+	c.b = byte((u>>8)&0xff)
+}
+
+func (c PL2Color) BGRA() uint32 {
+	return uint32(c.b)<<8 | uint32(c.g)<<16 | uint32(c.r)<<24 | uint32(0xff)
+}
+
+func (c PL2Color) SetBGRA(u uint32) {
+	c.b = byte((u>>24)&0xff)
+	c.g = byte((u>>16)&0xff)
+	c.r = byte((u>>8)&0xff)
+}
+
+
+
