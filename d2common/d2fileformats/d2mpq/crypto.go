540cc4364f39df7683cf0e9651cc4bb2d4dc1f95
Intyre <intyre@gmail.com>
2021-01-08 21:00:04 +0100

    d2mpq refactor: less linter noise

--- d2common/d2fileformats/d2mpq/crypto.go
+++ d2common/d2fileformats/d2mpq/crypto.go
@@ -19,7 +19,7 @@ func cryptoLookup(index uint32) uint32 {
 	return cryptoBuffer[index]
 }
 
-//nolint:gomnd // magic cryptographic stuff here...
+//nolint:gomnd // Decryption magic
 func cryptoInitialize() {
 	seed := uint32(0x00100001)
 
@@ -37,46 +37,49 @@ func cryptoInitialize() {
 	}
 }
 
+//nolint:gomnd // Decryption magic
 func decrypt(data []uint32, seed uint32) {
-	seed2 := uint32(0xeeeeeeee) //nolint:gomnd // Decryption magic
+	seed2 := uint32(0xeeeeeeee)
 
 	for i := 0; i < len(data); i++ {
-		seed2 += cryptoLookup(0x400 + (seed & 0xff)) //nolint:gomnd // Decryption magic
+		seed2 += cryptoLookup(0x400 + (seed & 0xff))
 		result := data[i]
 		result ^= seed + seed2
 
 		seed = ((^seed << 21) + 0x11111111) | (seed >> 11)
-		seed2 = result + seed2 + (seed2 << 5) + 3 //nolint:gomnd // Decryption magic
+		seed2 = result + seed2 + (seed2 << 5) + 3
 		data[i] = result
 	}
 }
 
+//nolint:gomnd // Decryption magic
 func decryptBytes(data []byte, seed uint32) {
-	seed2 := uint32(0xEEEEEEEE) //nolint:gomnd // Decryption magic
+	seed2 := uint32(0xEEEEEEEE)
 	for i := 0; i < len(data)-3; i += 4 {
-		seed2 += cryptoLookup(0x400 + (seed & 0xFF)) //nolint:gomnd // Decryption magic
+		seed2 += cryptoLookup(0x400 + (seed & 0xFF))
 		result := binary.LittleEndian.Uint32(data[i : i+4])
 		result ^= seed + seed2
 		seed = ((^seed << 21) + 0x11111111) | (seed >> 11)
-		seed2 = result + seed2 + (seed2 << 5) + 3 //nolint:gomnd // Decryption magic
+		seed2 = result + seed2 + (seed2 << 5) + 3
 
-		data[i+0] = uint8(result & 0xff)         //nolint:gomnd // Decryption magic
-		data[i+1] = uint8((result >> 8) & 0xff)  //nolint:gomnd // Decryption magic
-		data[i+2] = uint8((result >> 16) & 0xff) //nolint:gomnd // Decryption magic
-		data[i+3] = uint8((result >> 24) & 0xff) //nolint:gomnd // Decryption magic
+		data[i+0] = uint8(result & 0xff)
+		data[i+1] = uint8((result >> 8) & 0xff)
+		data[i+2] = uint8((result >> 16) & 0xff)
+		data[i+3] = uint8((result >> 24) & 0xff)
 	}
 }
 
+//nolint:gomnd // Decryption magic
 func decryptTable(r io.Reader, size uint32, name string) ([]uint32, error) {
 	seed := hashString(name, 3)
-	seed2 := uint32(0xEEEEEEEE) //nolint:gomnd // Decryption magic
+	seed2 := uint32(0xEEEEEEEE)
 	size *= 4
 
 	table := make([]uint32, size)
 	buf := make([]byte, 4)
 
 	for i := uint32(0); i < size; i++ {
-		seed2 += cryptoBuffer[0x400+(seed&0xff)] //nolint:gomnd // Decryption magic
+		seed2 += cryptoBuffer[0x400+(seed&0xff)]
 
 		if _, err := r.Read(buf); err != nil {
 			return table, err
@@ -86,7 +89,7 @@ func decryptTable(r io.Reader, size uint32, name string) ([]uint32, error) {
 		result ^= seed + seed2
 
 		seed = ((^seed << 21) + 0x11111111) | (seed >> 11)
-		seed2 = result + seed2 + (seed2 << 5) + 3 //nolint:gomnd // Decryption magic
+		seed2 = result + seed2 + (seed2 << 5) + 3
 		table[i] = result
 	}
 
@@ -98,30 +101,31 @@ func hashFilename(key string) uint64 {
 	return uint64(a)<<32 | uint64(b)
 }
 
+//nolint:gomnd // Decryption magic
 func hashString(key string, hashType uint32) uint32 {
-	seed1 := uint32(0x7FED7FED) //nolint:gomnd // Decryption magic
-	seed2 := uint32(0xEEEEEEEE) //nolint:gomnd // Decryption magic
+	seed1 := uint32(0x7FED7FED)
+	seed2 := uint32(0xEEEEEEEE)
 
 	/* prepare seeds. */
 	for _, char := range strings.ToUpper(key) {
 		seed1 = cryptoLookup((hashType*0x100)+uint32(char)) ^ (seed1 + seed2)
-		seed2 = uint32(char) + seed1 + seed2 + (seed2 << 5) + 3 //nolint:gomnd // Decryption magic
+		seed2 = uint32(char) + seed1 + seed2 + (seed2 << 5) + 3
 	}
 
 	return seed1
 }
 
-//nolint:unused,deadcode // will use this for creating mpq's
+//nolint:unused,deadcode,gomnd // will use this for creating mpq's
 func encrypt(data []uint32, seed uint32) {
-	seed2 := uint32(0xeeeeeeee) //nolint:gomnd // Decryption magic
+	seed2 := uint32(0xeeeeeeee)
 
 	for i := 0; i < len(data); i++ {
-		seed2 += cryptoLookup(0x400 + (seed & 0xff)) //nolint:gomnd // Decryption magic
+		seed2 += cryptoLookup(0x400 + (seed & 0xff))
 		result := data[i]
 		result ^= seed + seed2
 
 		seed = ((^seed << 21) + 0x11111111) | (seed >> 11)
-		seed2 = data[i] + seed2 + (seed2 << 5) + 3 //nolint:gomnd // Decryption magic
+		seed2 = data[i] + seed2 + (seed2 << 5) + 3
 		data[i] = result
 	}
 }
