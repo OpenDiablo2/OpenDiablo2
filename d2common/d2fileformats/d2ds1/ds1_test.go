4e7ec3e8430212076470745f9739578ccf788d5f
gucio321 <73652197+gucio321@users.noreply.github.com>
2021-02-23 21:35:06 +0100

    Ds1 refactor: removed npcIndexes field+fixed SetNumberOfWalls bug (#7)
    * ds1 refactor: removed npcIndexes field
    it was unnecessary, because described a number of objects with paths to use in encoder, but we can calculate manually
    
    * ds1 refactor: fixed set number of (layers) bug
    
    * ds1 refactor: SetNumberOf...Layers now returns error if incorrect number given
    
    * ds1 refactor: lintfix
    
    * ds1 refactor: rename: setupStreamLayerTypes -> GetStreamLayerTypes
    
    Co-authored-by: M. Sz <mszeptuch@protonmail.com>

--- d2common/d2fileformats/d2ds1/ds1_test.go
+++ d2common/d2fileformats/d2ds1/ds1_test.go
@@ -56,7 +56,6 @@ func exampleDS1() *DS1 {
 		numberOfFloorLayers:        1,
 		numberOfShadowLayers:       1,
 		numberOfSubstitutionLayers: 1,
-		npcIndexes:                 []int{},
 	}
 }
 
@@ -490,11 +489,16 @@ func TestDS1_NumberOfWalls(t *testing.T) {
 }
 
 func TestDS1_SetNumberOfWalls(t *testing.T) {
+	var err error
+
 	ds1 := exampleDS1()
 
-	newNumber := int32(2)
+	newNumber := int32(4)
 
-	ds1.SetNumberOfWallLayers(newNumber)
+	err = ds1.SetNumberOfWallLayers(newNumber)
+	if err != nil {
+		t.Errorf("error setting number of walls: %v", err)
+	}
 
 	test := func(ds1 *DS1) {
 		if len(ds1.tiles[0][0].Walls) != int(newNumber) {
@@ -506,13 +510,16 @@ func TestDS1_SetNumberOfWalls(t *testing.T) {
 		}
 	}
 
-	if err := testIfRestorable(ds1, test); err != nil {
+	if err = testIfRestorable(ds1, test); err != nil {
 		t.Errorf("unable to restore: %v", err)
 	}
 
 	newNumber = 1
 
-	ds1.SetNumberOfWallLayers(newNumber)
+	err = ds1.SetNumberOfWallLayers(newNumber)
+	if err != nil {
+		t.Errorf("error setting number of walls: %v", err)
+	}
 
 	test = func(ds1 *DS1) {
 		if len(ds1.tiles[0][0].Walls) != int(newNumber) {
@@ -524,7 +531,7 @@ func TestDS1_SetNumberOfWalls(t *testing.T) {
 		}
 	}
 
-	if err := testIfRestorable(ds1, test); err != nil {
+	if err = testIfRestorable(ds1, test); err != nil {
 		t.Errorf("unable to restore: %v", err)
 	}
 }
@@ -538,11 +545,16 @@ func TestDS1_NumberOfFloors(t *testing.T) {
 }
 
 func TestDS1_SetNumberOfFloors(t *testing.T) {
+	var err error
+
 	ds1 := exampleDS1()
 
 	newNumber := int32(2)
 
-	ds1.SetNumberOfFloorLayers(newNumber)
+	err = ds1.SetNumberOfFloorLayers(newNumber)
+	if err != nil {
+		t.Errorf("error setting number of floors: %v", err)
+	}
 
 	test := func(ds1 *DS1) {
 		if len(ds1.tiles[0][0].Floors) != int(newNumber) {
@@ -554,13 +566,16 @@ func TestDS1_SetNumberOfFloors(t *testing.T) {
 		}
 	}
 
-	if err := testIfRestorable(ds1, test); err != nil {
+	if err = testIfRestorable(ds1, test); err != nil {
 		t.Errorf("unable to restore: %v", err)
 	}
 
 	newNumber = 1
 
-	ds1.SetNumberOfFloorLayers(newNumber)
+	err = ds1.SetNumberOfFloorLayers(newNumber)
+	if err != nil {
+		t.Errorf("error setting number of floors: %v", err)
+	}
 
 	test = func(ds1 *DS1) {
 		if len(ds1.tiles[0][0].Floors) != int(newNumber) {
@@ -572,7 +587,7 @@ func TestDS1_SetNumberOfFloors(t *testing.T) {
 		}
 	}
 
-	if err := testIfRestorable(ds1, test); err != nil {
+	if err = testIfRestorable(ds1, test); err != nil {
 		t.Errorf("unable to restore: %v", err)
 	}
 }
@@ -625,7 +640,7 @@ func TestDS1_SetSubstitutionGroups(t *testing.T) {
 	}
 }
 
-func TestDS1_setupStreamLayerTypes(t *testing.T) {
+func TestDS1_GetStreamLayerTypes(t *testing.T) {
 	ds1 := exampleDS1()
 
 	lst := []d2enum.LayerStreamType{
@@ -636,7 +651,7 @@ func TestDS1_setupStreamLayerTypes(t *testing.T) {
 		d2enum.LayerStreamSubstitute,
 	}
 
-	layerStreamTypes := ds1.setupStreamLayerTypes()
+	layerStreamTypes := ds1.GetStreamLayerTypes()
 
 	if len(lst) != len(layerStreamTypes) {
 		t.Fatal("unexpected length")
