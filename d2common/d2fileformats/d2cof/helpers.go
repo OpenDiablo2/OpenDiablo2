15d30ffcce6d8d1e8362e9793bfde27de1c912a1
M. Sz <mszeptuch@protonmail.com>
2021-02-25 20:21:44 +0100

    cof: rename SpeedToFPS -> FPS

--- d2common/d2fileformats/d2cof/helpers.go
+++ d2common/d2fileformats/d2cof/helpers.go
@@ -1,7 +1,7 @@
 package d2cof
 
-// SpeedToFPS returns FPS value basing on cof's speed
-func (c *COF) SpeedToFPS() float64 {
+// FPS returns FPS value basing on cof's speed
+func (c *COF) FPS() float64 {
 	const (
 		baseFPS      = 25
 		speedDivisor = 256
