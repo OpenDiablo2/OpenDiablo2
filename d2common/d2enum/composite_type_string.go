36f356d0c3f7b7dbee545342c1b0d7338dcd6fc5
M. Sz <mszeptuch@protonmail.com>
2021-03-11 08:35:14 +0100

    d2enum: draw effect string

--- d2common/d2enum/composite_type_string.go
+++ d2common/d2enum/composite_type_string.go
@@ -37,3 +37,33 @@ func (i CompositeType) String() string {
 	}
 	return _CompositeType_name[_CompositeType_index[i]:_CompositeType_index[i+1]]
 }
+
+// Name returns a full name of layer
+func (i CompositeType) Name() string {
+	strings := map[CompositeType]string{
+		CompositeTypeHead:      "Head",
+		CompositeTypeTorso:     "Torso",
+		CompositeTypeLegs:      "Legs",
+		CompositeTypeRightArm:  "Right Arm",
+		CompositeTypeLeftArm:   "Left Arm",
+		CompositeTypeRightHand: "Right Hand",
+		CompositeTypeLeftHand:  "Left Hand",
+		CompositeTypeShield:    "Shield",
+		CompositeTypeSpecial1:  "Special 1",
+		CompositeTypeSpecial2:  "Special 2",
+		CompositeTypeSpecial3:  "Special 3",
+		CompositeTypeSpecial4:  "Special 4",
+		CompositeTypeSpecial5:  "Special 5",
+		CompositeTypeSpecial6:  "Special 6",
+		CompositeTypeSpecial7:  "Special 7",
+		CompositeTypeSpecial8:  "Special 8",
+	}
+
+	layerName, found := strings[i]
+	if !found {
+		return "Unknown"
+	}
+
+	return layerName
+
+}
