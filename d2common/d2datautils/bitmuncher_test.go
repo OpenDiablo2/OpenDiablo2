3b26d825d254ceae51a7bdea08054d41cd92e8f3
Will Roberts <willroberts3@protonmail.com>
2021-03-23 22:38:47 -0400

    Replaces comparisons with assertions

--- d2common/d2datautils/bitmuncher_test.go
+++ d2common/d2datautils/bitmuncher_test.go
@@ -1,6 +1,10 @@
 package d2datautils
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 var testData = []byte{33, 23, 4, 33, 192, 243}
 
@@ -9,9 +13,7 @@ func TestBitmuncherCopy(t *testing.T) {
 	bm2 := CopyBitMuncher(bm1)
 
 	for i := range bm1.data {
-		if bm1.data[i] != bm2.data[i] {
-			t.Fatal("original bitmuncher isn't equal to copied")
-		}
+		assert.Equal(t, bm1.data[i], bm2.data[i], "original bitmuncher isn't equal to copied")
 	}
 }
 
@@ -19,18 +21,14 @@ func TestBitmuncherSetOffset(t *testing.T) {
 	bm := CreateBitMuncher(testData, 0)
 	bm.SetOffset(5)
 
-	if bm.Offset() != 5 {
-		t.Fatal("Set Offset method didn't set offset to expected number")
-	}
+	assert.Equal(t, bm.Offset(), 5, "Set Offset method didn't set offset to expected number")
 }
 
 func TestBitmuncherSteBitsRead(t *testing.T) {
 	bm := CreateBitMuncher(testData, 0)
 	bm.SetBitsRead(8)
 
-	if bm.BitsRead() != 8 {
-		t.Fatal("Set bits read method didn't set bits read to expected value")
-	}
+	assert.Equal(t, bm.BitsRead(), 8, "Set bits read method didn't set bits read to expected value")
 }
 
 func TestBitmuncherReadBit(t *testing.T) {
@@ -43,55 +41,37 @@ func TestBitmuncherReadBit(t *testing.T) {
 		result |= byte(v) << byte(i)
 	}
 
-	if result != testData[0] {
-		t.Fatal("result of rpeated 8 times get bit didn't return expected byte")
-	}
+	assert.Equal(t, result, testData[0], "result of rpeated 8 times get bit didn't return expected byte")
 }
 
 func TestBitmuncherGetBits(t *testing.T) {
 	bm := CreateBitMuncher(testData, 0)
 
-	if byte(bm.GetBits(bitsPerByte)) != testData[0] {
-		t.Fatal("get bits didn't return expected value")
-	}
+	assert.Equal(t, byte(bm.GetBits(bitsPerByte)), testData[0], "get bits didn't return expected value")
 }
 
 func TestBitmuncherGetNoBits(t *testing.T) {
 	bm := CreateBitMuncher(testData, 0)
 
-	if bm.GetBits(0) != 0 {
-		t.Fatal("get bits didn't return expected value: 0")
-	}
+	assert.Equal(t, bm.GetBits(0), uint32(0), "get bits didn't return expected value: 0")
 }
 
 func TestBitmuncherGetSignedBits(t *testing.T) {
 	bm := CreateBitMuncher(testData, 0)
 
-	result := bm.GetSignedBits(6)
-	expected := -31
-	if result != expected {
-		t.Fatal("get signed bits didn't return expected value", result, expected)
-	}
+	assert.Equal(t, bm.GetSignedBits(6), -31, "get signed bits didn't return expected value")
 }
 
 func TestBitmuncherGetNoSignedBits(t *testing.T) {
 	bm := CreateBitMuncher(testData, 0)
 
-	result := bm.GetSignedBits(0)
-	expected := 0
-	if result != expected {
-		t.Fatal("get signed bits didn't return expected value", result, expected)
-	}
+	assert.Equal(t, bm.GetSignedBits(0), 0, "get signed bits didn't return expected value")
 }
 
 func TestBitmuncherGetOneSignedBit(t *testing.T) {
 	bm := CreateBitMuncher(testData, 0)
 
-	result := bm.GetSignedBits(1)
-	expected := -1
-	if result != expected {
-		t.Fatal("get signed bits didn't return expected value", result, expected)
-	}
+	assert.Equal(t, bm.GetSignedBits(1), -1, "get signed bits didn't return expected value")
 }
 
 func TestBitmuncherSkipBits(t *testing.T) {
@@ -99,9 +79,7 @@ func TestBitmuncherSkipBits(t *testing.T) {
 
 	bm.SkipBits(bitsPerByte)
 
-	if bm.GetByte() != testData[1] {
-		t.Fatal("skipping 8 bits didn't moved bit muncher's position into next byte")
-	}
+	assert.Equal(t, bm.GetByte(), testData[1], "skipping 8 bits didn't moved bit muncher's position into next byte")
 }
 
 func TestBitmuncherGetInt32(t *testing.T) {
@@ -113,9 +91,7 @@ func TestBitmuncherGetInt32(t *testing.T) {
 		testInt |= int32(testData[i]) << int32(bitsPerByte*i)
 	}
 
-	if bm.GetInt32() != testInt {
-		t.Fatal("int32 value wasn't returned properly")
-	}
+	assert.Equal(t, bm.GetInt32(), testInt, "int32 value wasn't returned properly")
 }
 
 func TestBitmuncherGetUint32(t *testing.T) {
@@ -127,7 +103,5 @@ func TestBitmuncherGetUint32(t *testing.T) {
 		testUint |= uint32(testData[i]) << uint32(bitsPerByte*i)
 	}
 
-	if bm.GetUInt32() != testUint {
-		t.Fatal("uint32 value wasn't returned properly")
-	}
+	assert.Equal(t, bm.GetUInt32(), testUint, "uint32 value wasn't returned properly")
 }
