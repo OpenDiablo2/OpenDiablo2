db99c66d2513c985206da8fc44c7222e8eb609f8
Panagiotis Georgiadis <pgeorgia@redhat.com>
2021-03-25 02:29:16 +0100

    Comply with gofmt -s

--- d2common/d2datautils/stream_reader.go
+++ d2common/d2datautils/stream_reader.go
@@ -85,7 +85,6 @@ func (v *StreamReader) ReadUInt64() (uint64, error) {
 		return 0, err
 	}
 
-
 	return uint64(b[0]) | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24 |
 		uint64(b[4])<<32 | uint64(b[5])<<40 | uint64(b[6])<<48 | uint64(b[7])<<56, err
 }
